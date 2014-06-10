6cba89c315a6185b5696651397766b12a918f3e1
Sébastien Hinderer <Sebastien.Hinderer@inria.fr>
2014-06-10 13:38:46 +0200

    In fun.lms.urls, static() should be called in DEBUG mode only.

--- fun/lms/urls.py
+++ fun/lms/urls.py
@@ -7,5 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = patterns( '', 
   (r'^university/', include('universities.urls')),
   (r'^', include('lms.urls'))
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
