3316510f512148c2c7066f1443f80808d7300697
Richard Moch <richard.moch@gmail.com>
2014-06-16 14:10:58 +0200

    Also serve media files in DEBUG mode

--- fun/lms/urls.py
+++ fun/lms/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls.static import static
 
 # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = patterns( '', 
-  (r'^university/', include('universities.urls')),
-  (r'^contact/', include('contact.urls')),
-  (r'^', include('lms.urls'))
+urlpatterns = patterns( '',
+    (r'^university/', include('universities.urls')),
+    (r'^contact/', include('contact.urls')),
+    (r'^', include('lms.urls'))
 )
 
 if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
