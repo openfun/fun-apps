ea0fed86b23114e37d26771b0af6c402a338ce41
Richard Moch <richard.moch@openfun.fr>
2014-07-16 10:02:54 +0000

    fun urls: import handler404 and handler500 from lms as they are not url patterns

--- fun/lms/urls.py
+++ fun/lms/urls.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
 from django.conf.urls import url, include, patterns
 from django.conf.urls.static import static
+
+from lms.urls import handler404, handler500
 
 # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = patterns( '',
     (r'^university/', include('universities.urls')),
     (r'^contact/', include('contact.urls')),
-    (r'^', include('lms.urls'))
+    (r'^', include('lms.urls')),
+
 )
 
 if settings.DEBUG:
