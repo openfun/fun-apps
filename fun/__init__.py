6db69242cd2f1f084bb5f8cb3de8b6539f2e5081
Richard Moch <richard.moch@gmail.com>
2014-07-18 13:13:35 +0200

    #577: monkeypatch edX to use our video component

---
+++ fun/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from django.utils.translation import ugettext as _
+
+from contentstore.views.component import _load_mixed_class, get_component_templates as edx_get_component_templates
+from xblock.plugin import PluginMissingError
+
+
+def fun_get_component_templates(course):
+    """Monkey patch contentstore.views.component.contentstore.views.component.get_component_templates function
+    to remove Youtube edX component and replace by our own Dailymotion xblock.
+    """
+
+    # retrieve original function result
+    component_templates = edx_get_component_templates(course)
+
+    # remove edX video component
+    component_templates = [component for component in component_templates
+            if component['type'] != 'video']
+
+    dmcloud_component_template = {"type": "video", "templates": [],
+            "display_name": _("Video")}
+    dmcloud_display_name = _load_mixed_class('dmcloud').display_name.default
+    dmcloud_component_template['templates'].append({
+        'display_name': dmcloud_display_name,
+        'category': 'dmcloud',
+        'boilerplate_name': None,
+        'is_common': False
+    })
+    component_templates.append(dmcloud_component_template)
+    return component_templates
+
+from contentstore.views import component
+component.get_component_templates = fun_get_component_templates
