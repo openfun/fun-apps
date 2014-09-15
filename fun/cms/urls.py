d7a5e2aed076241f1fb97f7dd3215b4c13cdf50d
Julien Paillé <julien.paille75@gmail.com>
2014-09-15 09:07:30 +0000

    #675 correct comment

--- fun/cms/urls.py
+++ fun/cms/urls.py
@@ -2,8 +2,9 @@
 
 from django.conf.urls import url, include, patterns
 
-# Behind the scene Django determines the 404 view by looking for handler404,
-# handler500 in your root URLconf. That why we import handlers here.
+# Behind the scene Django determines the 404 view and the 500 view
+# by looking for handler404, handler500 in your root URLconf.
+# That's why we import handlers here.
 from cms.urls import handler404, handler500
 
 urlpatterns = patterns( '',
