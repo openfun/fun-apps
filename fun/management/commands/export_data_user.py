8a545afbe05616368f21356ec1e3066b6afe1eca
Nicolas CAN <nicolas.can@univ-lille1.fr>
2014-10-23 10:25:46 +0000

    add pep8 except E265 and E501

--- fun/management/commands/export_data_user.py
+++ fun/management/commands/export_data_user.py
@@ -8,7 +8,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core import serializers
 from django.core.mail import EmailMultiAlternatives
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 from django.conf import settings
 from optparse import make_option
 from django.db.models.query import QuerySet
@@ -26,7 +27,7 @@ from student.models import UserProfile
 logger = logging.getLogger(__name__)
 
 
-#to run it just do ~/edx-platform$ ./manage.py lms export_data_user --settings=fun.lms_sloop --username=the_username
+# to run it just do ~/edx-platform$ ./manage.py lms export_data_user --settings=fun.lms_sloop --username=the_username
 
 class Command(BaseCommand):
     help = """
@@ -40,33 +41,36 @@ class Command(BaseCommand):
         [--host=127.0.0.1][--user_mongo=user_for_mongo][--pwd_mongo=password_for_mongo]
         http://edx.readthedocs.org/en/latest/internal_data_formats/sql_schema.html
         http://edx.readthedocs.org/en/latest/internal_data_formats/discussion_data.html
-
     """
     option_list = BaseCommand.option_list + (
         make_option(
             "--username",
-            dest = "username",
-            help = "the username"
+            dest="username",
+            help="the username"
         ),
         make_option(
             "--file",
-            dest = "file",
-            help = "the file to write"
+            dest="file",
+            help="the file to write",
+            default=""
         ),
         make_option(
             "--host",
-            dest = "host",
-            help = "the ip host adress for mongo connexion"
+            dest="host",
+            help="the ip host adress for mongo connexion",
+            default="127.0.0.1"
         ),
         make_option(
             "--user_mongo",
-            dest = "user_mongo",
-            help = "the username for mongo connexion"
+            dest="user_mongo",
+            help="the username for mongo connexion",
+            default=""
         ),
         make_option(
             "--pwd_mongo",
-            dest = "pwd_mongo",
-            help = "the password for mongo connexion"
+            dest="pwd_mongo",
+            help="the password for mongo connexion",
+            default=""
         ),
         make_option(
             "--email",
@@ -76,13 +80,11 @@ class Command(BaseCommand):
         ),
     )
 
-
-
     def handle(self, *args, **options):
-        if options['username'] == None :
+        if options['username'] is None:
             raise CommandError("Option `--username=...` must be specified.")
-        if options['file'] == None :
-            filename = "export_%s.log" %options['username']
+        if options['file'] is None or options['file'] == "":
+            filename = "export_%s.log" % options['username']
         else:
             filename = options['file']
         filename = '/tmp/%s' % filename
@@ -91,26 +93,25 @@ class Command(BaseCommand):
             user = User.objects.get(username=options['username'])
             profile = UserProfile.objects.get(user=user)
         except:
-            raise CommandError("User with username `%s` not found." %options['username'])
+            raise CommandError("User and/or profile with username `%s` not found." % options['username'])
 
         host = '127.0.0.1'
         if options['host']:
-            host=options['host']
-            print u"Using %s as host address" %host
-
+            host = options['host']
+            print u"Using %s as host address" % host
 
         user_mongo = None
         password_mongo = None
-        if options['user_mongo']:
-            user_mongo=options['user_mongo']
-            password_mongo=options['pwd_mongo']
+        if options['user_mongo'] and options['user_mongo'] != "":
+            user_mongo = options['user_mongo']
+            password_mongo = options['pwd_mongo']
             print u"With user %s" % user_mongo
 
-        all_models = models.get_models(include_auto_created=True) #return all models found
-
+        #return all models found
+        all_models = models.get_models(include_auto_created=True)
         with open(filename, 'wt') as f:
-            myfile = File(f)
-            printer = PrettyPrinter(stream=myfile, indent=2, width=1024, depth=None)
+            my_file = File(f)
+            printer = PrettyPrinter(stream=my_file, indent=2, width=1024, depth=None)
             #SQL DATA
             #TABLE USER:
             printer.pprint("Table User :")
@@ -122,20 +123,21 @@ class Command(BaseCommand):
             #printer.pprint(to_dict(user, exclude=('id', 'User.password')))
             printer.pprint("--")
             #OTHER TABLE
-            for model in all_models: #parse all models to find which one has a foreign key with User
+            #parse all models to find which one has a foreign key with User
+            for model in all_models:
                 for field in model._meta.fields:
-                    if field.get_internal_type()=="ForeignKey" and field.rel.to==User:
-                        printer.pprint("Table %s :" %model.__name__)
+                    if field.get_internal_type() == "ForeignKey" and field.rel.to == User:
+                        printer.pprint("Table %s :" % model.__name__)
                         #OR if isinstance(field, models.ForeignKey)
                         kwargs = {field.name: user}
-                        qs = model.objects.select_related().filter(**kwargs) #.values()
+                        qs = model.objects.select_related().filter(**kwargs)
                         if qs:
                             for q in qs:
                                 #print to_dict(q, exclude=('id', 'User.password'))
                                 printer.pprint(to_dict(q, exclude=('id', 'User.password')))
                                 #http://palewi.re/posts/2009/09/04/django-recipe-pretty-print-objects-and-querysets/
                                 #printer.pprint(qs)
-                                #dprint(qs, stream=myfile, indent=1, width=80, depth=None)
+                                #dprint(qs, stream=my_file, indent=1, width=80, depth=None)
                         else:
                             printer.pprint("No data found for this user")
                         printer.pprint("--")
@@ -145,27 +147,27 @@ class Command(BaseCommand):
             db = client.cs_comments_service
             if user_mongo:
                 db.authenticate(user_mongo, password_mongo)
-            listpost = db.contents.find({"author_id":"%s" % user.id})
+            list_post = db.contents.find({"author_id": "%s" % user.id})
             printer.pprint("Discussions :")
-            printer.pprint("Nombre d'entree : %s" %listpost.count())
-            for post in listpost:
+            printer.pprint("Nombre d'entree : %s" % list_post.count())
+            for post in list_post:
                 printer.pprint(post)
 
-            printer.pprint(20*"*")
+            printer.pprint(20 * "*")
 
         if options['email']:
             context = {}
             context['subject'] = u"[FUN] Export des données de l'utilisateur %s au %s" % (
-                    options['username'], datetime.now().strftime('%d/%m/%Y'))
+                options['username'], datetime.now().strftime('%d/%m/%Y'))
             html_content = render_to_string('fun/emails/base_email.html', context)
             text_content = "This is a HTML only email"
 
             email = EmailMultiAlternatives(
-                    subject=context['subject'],
-                    body=text_content,
-                    from_email=settings.SERVER_EMAIL,
-                    to=[options['email']],
-                )
+                subject=context['subject'],
+                body=text_content,
+                from_email=settings.SERVER_EMAIL,
+                to=[options['email']],
+            )
             email.attach_alternative(html_content, "text/html")
             email.attach_file(filename)
             try:
@@ -174,27 +176,29 @@ class Command(BaseCommand):
                 logger.error(u"Stat email could not be sent(%s)." % subject)
                 print u"Unexpected error append while sending %s" % filename
 
+
 def to_dict(obj, exclude=[]):
     """
     """
     tree = {}
     for field in obj._meta.fields + obj._meta.many_to_many:
         if field.name in exclude or \
-           '%s.%s' % (type(obj).__name__, field.name) in exclude:
+                '%s.%s' % (type(obj).__name__, field.name) in exclude:
             continue
-
-        try :
+        try:
             value = getattr(obj, field.name)
         except obj.DoesNotExist:
             value = None
 
         if type(field) in [models.ForeignKey, models.OneToOneField]:
-            tree[field.name] = "%s" %value #to_dict(value, exclude=exclude)
+            #to_dict(value, exclude=exclude)
+            tree[field.name] = "%s" % value
         elif isinstance(field, models.ManyToManyField):
-            vs = []
-            for v in value.all():
-                vs.append(v) #to_dict(v, exclude=exclude))
-            tree[field.name] = "%s" %vs
+            array_value = []
+            for val in value.all():
+                #to_dict(v, exclude=exclude))
+                array_value.append(val)
+            tree[field.name] = "%s" % array_value
         elif isinstance(field, models.DateTimeField):
             tree[field.name] = str(value)
         elif isinstance(field, models.FileField):
