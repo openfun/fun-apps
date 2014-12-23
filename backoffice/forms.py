4d669bd3d06f4a696ab7ac62d6bf46e681a8b724
Julien Paillé <julien.paille75@gmail.com>
2014-12-23 09:56:48 +0000

    issue959 refactoring commit

--- backoffice/forms.py
+++ backoffice/forms.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
-
 from django import forms
+from django.forms.formsets import BaseFormSet
 
-class TestCertificateForm(forms.Form):
-    full_name = forms.CharField(max_length=100)
+from django.utils.translation import ugettext_lazy as _
 
-    teacher1 = forms.CharField(max_length=100)
-    title1 = forms.CharField(max_length=100)
+class RequiredFormSet(BaseFormSet):
+    '''Make the first set Mandatory'''
+    def __init__(self, *args, **kwargs):
+        super(RequiredFormSet, self).__init__(*args, **kwargs)
+        self.forms[0].empty_permitted = False
 
-    teacher2 = forms.CharField(max_length=100,required=False)
-    title2 = forms.CharField(max_length=100, required=False)
+class StudentCertificateForm(forms.Form):
+    full_name = forms.CharField(max_length=100, label=_(u"Full name"))
 
-    teacher3 = forms.CharField(max_length=100, required=False)
-    title3 = forms.CharField(max_length=100, required=False)
-
-
-    teacher4 = forms.CharField(max_length=100, required=False)
-    title4 = forms.CharField(max_length=100, required=False)
-
-
-    def make_teachers_list(self):
-        '''Return a list of teacher/title, format required by the module generator.py'''
-        teachers = []
-
-        for key in range(1, 5):
-            if u"teacher{}".format(key) in self.cleaned_data and u"title{}".format(key) in self.cleaned_data:
-                teachers.append(u"{}/{}".format(self.cleaned_data[u"teacher{}".format(key)],
-                                               self.cleaned_data[u"title{}".format(key)]))
-        return teachers
+class TeachersCertificateForm(forms.Form):
+    MAX_TEACHERS = 4
+    full_name = forms.CharField(max_length=100, label=_(u"Full Name"))
+    title = forms.CharField(max_length=100, label=_(u"Title"))
