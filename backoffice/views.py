7aa418346f754e4b5942aa17a6116e7630120413
Julien Paillé <julien.paille75@gmail.com>
2014-12-23 09:56:48 +0000

    issue959 refactoring commit

--- backoffice/views.py
+++ backoffice/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
+from django.forms.formsets import formset_factory
 
 from courseware.courses import get_courses, sort_by_announcement
 from courseware.courses import course_image_url, get_course_about_section
@@ -18,7 +19,7 @@ from opaque_keys.edx.keys import CourseKey
 from xmodule.modulestore.django import modulestore
 from capa.xqueue_interface import make_hashkey
 
-from backoffice.forms import TestCertificateForm
+from backoffice.forms import StudentCertificateForm, TeachersCertificateForm, RequiredFormSet
 from fun_certificates.generator import CertificateInfo
 from universities.models import University
 
@@ -68,51 +69,62 @@ def course_detail(request, course_key_string):
     course = modulestore().get_course(ck, depth=0)
     setattr(course, 'ident', course.id.to_deprecated_string())
 
+    teachers_form_set = formset_factory(TeachersCertificateForm,
+                                      extra=TeachersCertificateForm.MAX_TEACHERS,
+                                      formset=RequiredFormSet)
+    student_form = StudentCertificateForm()
+
     if request.method == 'POST':
-        form = TestCertificateForm(request.POST)
-        if (form.is_valid()):
-            return generate_test_certificate(request, course, form)
-    else:
-        form = TestCertificateForm()
+        student_form = StudentCertificateForm(request.POST)
+        teachers_form_set = teachers_form_set(request.POST)
+        if (student_form.is_valid() and teachers_form_set.is_valid()):
+            try:
+                university = University.objects.get(code=course.org)
+            except:
+                messages.warning(request, _("University doesn't exist"))
+            certificate = generate_test_certificate(course, university, student_form, teachers_form_set)
+            if certificate:
+                response = HttpResponse("", content_type='text/pdf')
+                response['Content-Disposition'] = 'attachment; filename="{}"'.format(certificate.filename)
+                with open(certificate.pdf_file_name, 'r') as gradefile:
+                    response.write(gradefile.read())
+                return response
+            else:
+                messages.error(request, _('Certificated generation failed'))
+
     return render(request, 'backoffice/course.html', {
-        'course': course,
-        'form' : form,
-    })
+            'course': course,
+            'student_form_certificate' : student_form,
+            'teachers_form_certificate' : teachers_form_set,
+        })
 
-def generate_test_certificate(request, course, form):
-    """Generate the pdf certicate, save it on disk and then return the certificate as http response"""
-
-    try:
-        university = University.objects.get(code=course.org)
-    except:
-        messages.warning(request, _("University doesn't exist"))
-        return redirect(reverse('backoffice-course-detail', args=[course.id.to_deprecated_string()]))
-
-    if university.certificate_logo:
-        logo_path = os.path.join(university.certificate_logo.url, university.certificate_logo.path)
-    else:
-        logo_path = None
+def generate_test_certificate(course, university, student_form, teachers_form_set):
+    """Generate the pdf certicate, save it on disk"""
 
     certificate = CertificateInfo()
 
-    certificate.full_name = form.cleaned_data['full_name']
+    certificate.full_name = student_form.cleaned_data['full_name']
     certificate.course_name = course.display_name
     certificate.organization = course.org
-    certificate.teachers = form.make_teachers_list()
+
+    # make a teachers/title list from the teachers_form_set
+    certificate.teachers =  [u"{}/{}".format(teacher.cleaned_data['full_name'],
+                                            teacher.cleaned_data['title'],)  for teacher in teachers_form_set
+                             if 'full_name'  in teacher.cleaned_data and 'title' in teacher.cleaned_data]
+
+    if university.certificate_logo:
+        logo_path = os.path.join(university.certificate_logo.url, university.certificate_logo.path)
     certificate.organization_logo = logo_path
 
     key = make_hashkey(random.random())
-    certificate_filename = "TEST_attestation_suivi_%s_%s.pdf" % (
+    certificate.filename = "TEST_attestation_suivi_%s_%s.pdf" % (
             course.id.to_deprecated_string().replace('/','_'), key)
     certificate.pdf_file_name = os.path.join(
-        settings.CERTIFICATES_DIRECTORY, certificate_filename)
+        settings.CERTIFICATES_DIRECTORY, certificate.filename)
+
     if certificate.generate():
-        response = HttpResponse("", content_type='text/pdf')
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(certificate_filename)
-        with open(certificate.pdf_file_name, 'r') as gradefile:
-            response.write(gradefile.read())
-        return (response)
+        return certificate
     else:
-        messages.error(request, _('Certificated generation failed'))
-        return redirect(reverse("backoffice-course-detail", args=[course.id.to_deprecated_string()]))
+        return False
+
 
