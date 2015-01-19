ac90d1694a9bfcb7af07b4f9f400343831f37116
Richard Moch <richard.moch@gmail.com>
2015-01-19 09:43:21 +0000

    Fix context_processor

--- fun/context_processor.py
+++ fun/context_processor.py
@@ -5,8 +5,7 @@ from django.conf import settings
 
 def fun_settings(request):
     """Add ENVIRONEMENT name (Brick, Ketch, dev) to template context when in backoffice application."""
+    context = {}
     if request.path.startswith('/backoffice/'):
-        context = {
-            'ENVIRONEMENT': settings.ENVIRONEMENT,
-        }
-        return context
+        context['ENVIRONEMENT'] = settings.ENVIRONEMENT
+    return context
