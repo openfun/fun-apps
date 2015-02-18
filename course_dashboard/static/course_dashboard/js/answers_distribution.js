cda1af6cf9c9a20733724bae394f58f554bdba55
Julien Paillé <julien.paille75@gmail.com>
2015-02-18 14:51:14 +0000

    improve readibilty of answers_distribution.js file

--- course_dashboard/static/course_dashboard/js/answers_distribution.js
+++ course_dashboard/static/course_dashboard/js/answers_distribution.js
@@ -1,64 +1,64 @@
 function answers_distribution (static_url, get_answers_url, refresh_string) {
+    
+    var current_problem_module_id = ''
+    
+    var ajaxload_tag = "<img src='" + static_url + "course_dashboard/images/ajaxload.gif' class='ajax-load-img' alt='ajax loading button'>";
 
-    var current_module_id = ''
-    $('.problem-path').button().click(function(event) {
-
-	var module_id = this.getAttribute("module-id");
+    $('.problem-path').button().click(get_results);
+    
+    function get_results(event) {
 	
-	var ajaxload_tag = "<img class='" + module_id + "' src='" + static_url + "course_dashboard/images/ajaxload.gif' alt='ajax loading button' >";
+	if ($(this).hasClass('refresh-button'))
+	    var problem_module_id = this.parentNode.parentNode.id
+	else
+	    var problem_module_id = this.parentNode.id
+	
+	problem_module_selector = '.problem-module#' + problem_module_id
 
 	/*Toggle the chevron and show/hide results*/
-	if ($('li.' + module_id).hasClass('glyphicon-chevron-right')) {
-	    $('li.' + module_id).removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
-	    $('#' + module_id).show();
-	    $('[button-id=' + module_id +']').show();	    
-	 }
-	else {
-	    $('li.' + module_id).removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
-            $('#' + module_id).hide();
-	    $('[button-id=' + module_id +']').hide();	    
+	if (!$(this).hasClass('refresh-button')) {
+	    if ($(problem_module_selector + ' li.glyphicon').hasClass('glyphicon-chevron-right')) {
+		$(problem_module_selector + ' li.glyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
+		$(problem_module_selector + ' .results').show();
+		$(problem_module_selector + ' .refresh-button').show();
+	    }
+	    else {
+		$(problem_module_selector + ' li.glyphicon').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+		$(problem_module_selector + ' .results').hide();
+		$(problem_module_selector + ' .refresh-button').hide();
+	    }
 	}
 	
-	/*The ajax request is only done when we first unfold a problem module.
+	/*The ajax request is only done when we first unfold a problem module and when there is no other request going on.
           Then the refresh button is used.*/
-	if (this.getAttribute('clicked') == 'false' &&  current_module_id == '') {
+	if (current_problem_module_id == '') {
+	    if ($(this).hasClass('refresh-button') || !$(problem_module_selector + ' .refresh-button').length) {
 	    // perform the request
-	    $.get(get_answers_url, {'problem_module_id': module_id}, display_results);
+	    $.get(get_answers_url, {'problem_module_id': problem_module_id}, display_results);
 	    // insert ajax load tag
-	    $(ajaxload_tag).insertAfter(this)
-	    // save module_id in order to insert the result of get_answers request in the right module div
-	    current_module_id = module_id;
+	    $(ajaxload_tag).insertAfter(this) // TODO insert beetween two rows not good
+	    // save problem_module_id in order to insert the result of get_answers request in the right module div
+	    current_problem_module_id = problem_module_id;
+	    }
 	}
-    });
+    }
 
     function display_results(xml_data) {
 	// called after the get_answers/ request has returned
-	var problem_module = $('[module-id='+ current_module_id +']')
 	
+	problem_module_selector = '.problem-module#' + current_problem_module_id
 	// inject the xml response at the right place
-	$('#' + current_module_id).html(xml_data);
+	
+	$(problem_module_selector + ' .results').html(xml_data);
 	// remove ajaxload button
-        $('img.' + current_module_id).remove();
+        $(problem_module_selector + ' .ajax-load-img').remove();
 	// inject the refresh button tag if necessary
-	if (problem_module.attr('clicked') == 'false'){
-	    var refreshbutton_tag= " <a class='btn btn-success col-lg-offset-10' button-id='" + current_module_id +"'>" + refresh_string + "</a>";
-	    $(refreshbutton_tag).insertAfter('#' + current_module_id);
-	    problem_module.attr('clicked' , 'true');
+	if (!$(problem_module_selector + ' .refresh-button').length) {
+	    var refreshbutton_tag= "<a class='btn btn-success col-lg-offset-10 refresh-button'>" + refresh_string + "</a>";
+	    $(refreshbutton_tag).insertAfter($(problem_module_selector + ' .results'));
 	    // bind refresh button to refresh_results
-	    $('a.btn').button().click(refresh_results)
+	    $(problem_module_selector + ' .refresh-button').button().click(get_results)
 	}
-	current_module_id = '';
+	current_problem_module_id = '';
     }
-
-    function refresh_results(event) {
-	module_id = this.getAttribute('button-id');
-lol
-	var ajaxload_tag = "<img class='" + module_id + "' src='" + static_url + "course_dashboard/images/ajaxload.gif' alt='ajax loading button' >";
-	if (current_module_id == '') {
-	    $.get(get_answers_url, {'problem_module_id': module_id}, display_results);
-	    current_module_id = module_id;
-	    $(ajaxload_tag).insertAfter(this);
-	}
-    }
-};
-
+}
