1d39491a83a67f239ca2ae2d2e30a68d8c3410db
Richard Moch <richard.moch@gmail.com>
2015-03-24 15:03:04 +0000

    delete bad test

--- course_dashboard/tests/test_answers_distribution.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseCourseDashboardTestCase
-
-class AnswerDistributionTestCase(BaseCourseDashboardTestCase):
-    def test_one_response(self):
-        self.create_student_module()
-        
