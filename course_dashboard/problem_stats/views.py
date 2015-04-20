fc558ed07080bc67f45259e6064ec63f424975ef
Julien Paillé <julien.paille75@gmail.com>
2015-04-20 09:09:18 +0000

    Issue1395 Add tracking logs for answers distribution.

--- course_dashboard/problem_stats/views.py
+++ course_dashboard/problem_stats/views.py
@@ -1,16 +1,17 @@
 import json
+import time
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from eventtracking import tracker
 from opaque_keys.edx.keys import CourseKey
-
-from fun.utils.views import ensure_valid_course_key
-from fun.utils.views import staff_required_or_level
-
 from xmodule.modulestore.django import modulestore
+
 from course_dashboard.problem_stats import utils
 from course_dashboard.problem_stats.problem_monitor import ProblemMonitor
+from fun.utils.views import ensure_valid_course_key
+from fun.utils.views import staff_required_or_level
 
 @ensure_valid_course_key
 @staff_required_or_level('staff')
@@ -20,9 +21,13 @@ def index(request, course_id):
     Args:
          course_id (str): The course id as string.
     """
+    start_time = time.time()
+
     store = modulestore()
     course_key = CourseKey.from_string(course_id)
     course_tree = utils.build_course_tree(store.get_course(course_key))
+    tracker.emit("course_dashboard.problem_stats.views.index",
+                 {'task-time' : time.time() - start_time})
 
     return render(request, 'problem_stats/index.html', {
         "course_id": course_id,
@@ -39,11 +44,14 @@ def get_stats(request, course_id, problem_id):
     Args:
          course_id (str): The course id as string.
      """
+    start_time = time.time()
     store = modulestore()
     course_key = CourseKey.from_string(course_id)
 
     problem = utils.fetch_problem(store, course_key, problem_id)
     problem_monitor = ProblemMonitor(problem)
     problem_monitor.get_student_answers()
+    tracker.emit("course_dashboard.problem_stats.views.get_stats",
+                 {'task-time' : time.time() - start_time})
 
     return HttpResponse(problem_monitor.get_html())
