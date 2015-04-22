3cf17c5c8b099a2c1303f5fc6a779ad3c20ad677
Julien Paillé <julien.paille75@gmail.com>
2015-04-22 08:04:56 +0000

    Remove forgotten ipdb in template.
    + change module id format in url.

--- course_dashboard/problem_stats/urls.py
+++ course_dashboard/problem_stats/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('course_dashboard.problem_stats.views',
                        url(r'^index/$', 'index', name='index'),
-                       url(r'^get_stats/(?P<problem_id>[a-z0-9]{32})$', 'get_stats', name='get-stats'),
+                       url(r'^get_stats/(?P<problem_id>.*)$', 'get_stats', name='get-stats'),
 )
--- course_dashboard/reports_manager/urls.py
+++ course_dashboard/reports_manager/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('course_dashboard.reports_manager.views',
     url(r'^dashboard/$', 'dashboard', name='dashboard'),
-    url(r'^generate/(?P<problem_id>[a-z0-9]{32})/$', 'generate', name='generate'),
+    url(r'^generate/(?P<problem_id>.*)/$', 'generate', name='generate'),
     url(r'^download/(?P<answers_distribution_report>.+.csv)$', 'download', name='download'),
 )
 
