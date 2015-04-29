b8a442869184853b3b2187bb90ae6d4583da6b8c
Régis Behmo <regis@behmo.com>
2015-04-29 16:36:30 +0200

    Rewrite /fonts urls in debug mode
    This is the only way to load FontAwesome from non-compiled/compressed
    stylesheets.
    
    This closes issue #1107.

--- fun/common_urls.py
+++ fun/common_urls.py
@@ -8,6 +8,13 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         url(r'^__debug__/', include(debug_toolbar.urls)),
+
+        # Fonts are loaded from /fonts/ in DEBUG mode because the loaded
+        # stylesheets are not compiled/compressed.
+        url(r'^(?P<path>fonts/vendor/.+)$',
+            view='django.views.static.serve',
+            kwargs={'document_root' : settings.STATIC_ROOT}),
     )
