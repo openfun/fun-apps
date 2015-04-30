aabe0c6042d6022b0d61b6ec75045a4cb61e02f0
Régis Behmo <regis@behmo.com>
2015-04-30 16:06:44 +0200

    Disable max number of failed login attemps
    This feature should remain disabled while LoginFailures.user has no
    unique constraint, as it causes errors in fetching LoginFailure objects.
    
    This closes issue #1343.

--- fun/envs/cms/common.py
+++ fun/envs/cms/common.py
@@ -18,12 +18,6 @@ INSTALLED_APPS += (
 
 ROOT_URLCONF = 'fun.cms.urls'
 
-# Allow all courses to use advanced components
-FEATURES['ALLOW_ALL_ADVANCED_COMPONENTS'] = True
-
-# restrain user who can create course in studio to granted ones in CourseCreator table
-FEATURES['ENABLE_CREATOR_GROUP'] = True
-
 # LOGGING contant is populated by get_logger_config function called in edx's common settings.
 # For sorme reason, not configuring a remote syslogger makes a lot of
 # useless messages in logs (TypeError: getsockaddrarg: AF_INET address must be tuple, not NoneType)
@@ -45,15 +39,8 @@ del LOGGING['handlers']['newrelic']
 STATIC_URL = "/static/"
 STATIC_ROOT = "/edx/var/edxapp/staticfiles"
 
-
-# Enable admin site for university app
-#from devstack import FEATURES
-FEATURES['ENABLE_DJANGO_ADMIN_SITE'] = True
-
-
 # add 'theme/cms/templates' directory to MAKO template finder to override some CMS templates...
 MAKO_TEMPLATES['main'].insert(0, ENV_ROOT / 'fun-apps/fun/templates/cms')
-
 
 # #652 we need this to False for course HTML description to be editable
 # but it's for now incompatible with working footpage links
@@ -67,22 +54,25 @@ MKTG_URLS['ROOT'] = 'http://' + LMS_BASE
 MKTG_URLS['TOS'] = '/tos'
 MKTG_URLS['PRIVACY'] = '/privacy'
 
-
-# from former cms.env.json
+# Allow all courses to use advanced components
+FEATURES['ALLOW_ALL_ADVANCED_COMPONENTS'] = True
 FEATURES['AUTH_USE_OPENID_PROVIDER'] = True
 FEATURES['AUTOMATIC_AUTH_FOR_TESTING'] = False
 FEATURES['ADVANCED_SECURITY'] = False
 FEATURES['CERTIFICATES_ENABLED'] = True
+FEATURES['ENABLE_CONTENT_LIBRARIES'] = False  # Content libraries support requires new coursekey format
+# restrain user who can create course in studio to granted ones in CourseCreator table
+FEATURES['ENABLE_CREATOR_GROUP'] = True
 FEATURES['ENABLE_DISCUSSION_SERVICE'] = True
+FEATURES['ENABLE_DJANGO_ADMIN_SITE'] = True
 FEATURES['ENABLE_INSTRUCTOR_ANALYTICS'] = True
+FEATURES['ENABLE_MAX_FAILED_LOGIN_ATTEMPTS'] = False
 FEATURES['ENABLE_S3_GRADE_DOWNLOADS'] = True
+FEATURES['ENFORCE_PASSWORD_POLICY'] = True
+FEATURES['IS_EDX_DOMAIN'] = True  # used to display Edx Studio logo, see edx-platform/cms/templates/widgets/header.html
 FEATURES['SUBDOMAIN_BRANDING'] = False
 FEATURES['SUBDOMAIN_COURSE_LISTINGS'] = False
 FEATURES['USE_CUSTOM_THEME'] = False
-
-FEATURES['ENFORCE_PASSWORD_POLICY'] = True
-
-FEATURES['ENABLE_CONTENT_LIBRARIES'] = False  # disable content libraries support (need new coursekey format)
 
 CC_PROCESSOR = {
     'CyberSource': {
@@ -95,5 +85,3 @@ CC_PROCESSOR = {
 }
 
 SITE_VARIANT = 'cms'
-
-FEATURES['IS_EDX_DOMAIN'] = True  # used to display Edx Studio logo, see edx-platform/cms/templates/widgets/header.html
