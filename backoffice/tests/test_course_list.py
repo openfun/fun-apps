5a606b73ac7bf7908101b41222cbd7b1af4c4940
Richard Moch <richard.moch@openfun.fr>
2015-05-19 10:22:12 +0000

    Revert "Add test dailymotion ID"
    This reverts commit 01a114184060282eef8b2975898d42d025670742.

--- backoffice/tests/test_course_list.py
+++ backoffice/tests/test_course_list.py
@@ -7,16 +7,11 @@ from django.contrib.auth.models import User, Group
 from django.test.utils import override_settings
 from django.core.urlresolvers import reverse
 
-from opaque_keys.edx.keys import CourseKey
-from xmodule.modulestore.django import modulestore
-from xmodule.modulestore.exceptions import ItemNotFoundError
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory, CourseAboutFactory, ABOUT_ATTRIBUTES
 
 from student.models import UserProfile
 from universities.factories import UniversityFactory
-
-from ..views import get_about_sections
 
 
 class BaseCourseList(ModuleStoreTestCase):
@@ -41,7 +36,6 @@ class BaseCourseList(ModuleStoreTestCase):
                              display_name=u"published", ispublic=True)
         self.list_url = reverse('backoffice:courses-list')
 
-
 class TestExportCoursesList(BaseCourseList):
     def get_csv_response_rows(self, response):
         response_content = StringIO(response.content)
@@ -56,42 +50,3 @@ class TestExportCoursesList(BaseCourseList):
         course = rows[1]
         self.assertIn("www.youtube.com/embed/testing-video-link", course)
         self.assertIn(ABOUT_ATTRIBUTES['effort'], course)
-
-
-class TestDailymotionIDRetrieving(BaseCourseList):
-    """Test that our dailymotion ID is correctly extracted from course about section which is
-    made to store youtube ID. We copy/paste those 2 functions because we can't import
-    cms/djangoapps/models/settings/course_details.py but it reduce the scope of the test...
-    """
-    def _recompose_video_tag(self, video_key):
-        result = None
-        if video_key:
-            result = '<iframe width="560" height="315" src="//www.youtube.com/embed/' + \
-                video_key + '?rel=0" frameborder="0" allowfullscreen=""></iframe>'
-        return result
-    def _update_about_item(self, course_key, about_key, data, course, user):
-        temploc = course_key.make_usage_key('about', about_key)
-        store = modulestore()
-        if data is None:
-            try:
-                store.delete_item(temploc, user.id)
-            except ValueError:
-                pass
-        else:
-            try:
-                about_item = store.get_item(temploc)
-            except ItemNotFoundError:
-                about_item = store.create_xblock(course.runtime, course.id, 'about', about_key)
-            about_item.data = data
-            store.update_item(about_item, user.id, allow_not_found=True)
-
-    def test_dailymotion_extraction(self):
-        DM_CODE = 'x2an9mg'
-        module_store = modulestore()
-        descriptor = module_store.get_course(self.course2.id)
-        recomposed_video_tag = self._recompose_video_tag(DM_CODE)
-        self._update_about_item(self.course2.id, 'video', recomposed_video_tag, descriptor, self.user)
-
-        course_infos = get_about_sections(descriptor)
-
-        self.assertEqual(DM_CODE, course_infos['video'])
