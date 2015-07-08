576ccf5dfdeed7c7dafd9eb6270cbb4b300d8476
Richard Moch <richard@openfun.fr>
2015-07-08 16:51:41 +0000

    #1585 modify group_required recorator to support microsites

--- backoffice/tests/test_microsites.py
+++ backoffice/tests/test_microsites.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse
 
 from student.tests.factories import UserFactory
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 
 from fun.tests.utils import skipUnlessLms, setMicrositeTestSettings
 
@@ -25,13 +27,26 @@ FAKE_MICROSITE2 = {
     }
 
 
+class BaseMicrositeTestCase(ModuleStoreTestCase):
+    def _create_user(self, username, microsite):
+        """Create a user to log in the backoffice belonging to the given microsite."""
+        user = MicrositeUserFactory(user__username=username, site=microsite).user
+        user.set_password('password')
+        user.save()
+        backoffice_group, created = Group.objects.get_or_create(name='fun_backoffice')
+        user.groups.add(backoffice_group)
+        return user
+
+    def setUp(self):
+        super(BaseMicrositeTestCase, self).setUp(create_user=False)
+        self.backuser1 = self._create_user('backuser1', 'microsite1')
+        self.backuser2 = self._create_user('backuser2', 'microsite2')
+
+
 @skipUnlessLms
-class TestMicrositeUsers(BaseCourseList):
+class TestMicrositeUsers(BaseMicrositeTestCase):
     def setUp(self):
         super(TestMicrositeUsers, self).setUp()
-
-        self.fcourse1 = Course.objects.create(key=self.course1.id, university=self.university)
-        self.fcourse2 = Course.objects.create(key=self.course2.id, university=self.university)
 
         self.user0 = UserFactory(username='user0')
         self.user1 = MicrositeUserFactory(user__username='user1', site='microsite1').user
@@ -39,6 +54,7 @@ class TestMicrositeUsers(BaseCourseList):
 
     @setMicrositeTestSettings(FAKE_MICROSITE1)
     def test_user_list_with_microsite1(self):
+        self.client.login(username=self.backuser1.username, password='password')
         response = self.client.get(reverse('backoffice:user-list'))
         users = response.context['users'].object_list
         self.assertTrue(self.user1 in users)
@@ -47,6 +63,7 @@ class TestMicrositeUsers(BaseCourseList):
 
     @setMicrositeTestSettings(FAKE_MICROSITE2)
     def test_user_list_with_microsite2(self):
+        self.client.login(username=self.backuser2.username, password='password')
         response = self.client.get(reverse('backoffice:user-list'))
         users = response.context['users'].object_list
         self.assertTrue(self.user2 in users)
@@ -55,7 +72,15 @@ class TestMicrositeUsers(BaseCourseList):
 
     @setMicrositeTestSettings(FAKE_MICROSITE1)
     def test_user_edit_with_microsite(self):
+        self.client.login(username=self.backuser1.username, password='password')
         response = self.client.get(reverse('backoffice:user-detail', args=[self.user1.username]))
         self.assertEqual(200, response.status_code)
         response = self.client.get(reverse('backoffice:user-detail', args=[self.user2.username]))
         self.assertEqual(404, response.status_code)
+
+    @setMicrositeTestSettings(FAKE_MICROSITE1)
+    def test_microsite_acces_controle(self):
+        """Backoffice user from microsite1 should not acces to microsite2 backoffice."""
+        self.client.login(username=self.backuser2.username, password='password')
+        response = self.client.get(reverse('backoffice:user-list'))
+        self.assertEqual(302, response.status_code)  # should be redirected to login page
