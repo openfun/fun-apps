7b4423ea36026bc3468477c11526ed3a80259450
Régis Behmo <regis@behmo.com>
2015-07-22 11:47:50 +0200

    Handle underscore templates in makemessages_fun
    Two different .po files need to be generated for each app: django.po and
    djangojs.po. If either of these files does not exist, it is simply not
    processed by makemessages_fun. Thus, the former behaviour is preserved.
    However, makemessages_fun will no longer crash if a .po file is missing;
    instead, a warning will be printed.

--- fun/management/commands/makemessages_fun.py
+++ fun/management/commands/makemessages_fun.py
@@ -18,11 +18,10 @@ except ImportError:
 
 CURRENT_DIR = os.path.dirname(__file__)
 FUN_APPS_ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "../../../"))
-PATH_FUN_DJANGOPO = "%(root_path)s/locale/%(locale)s/LC_MESSAGES/django.po"
-PATH_EDX_DJANGOPO = os.path.join(
-    os.path.dirname(__file__),
-    "../../../../edx-platform/",
-    "conf/locale/%(locale)s/LC_MESSAGES/django.po"
+PATH_FUN_PO = "%(root_path)s/locale/%(locale)s/LC_MESSAGES/%(domain)s.po"
+PATH_EDX_PO = os.path.join(
+    FUN_APPS_ROOT_DIR, "../edx-platform/",
+    "conf/locale/%(locale)s/LC_MESSAGES/%(domain)s.po"
 )
 
 
@@ -30,7 +29,7 @@ class Command(BaseCommand):
 
     help = """Usage: makemessages_fun [-v] [-l fr|de] path1 [path2 [...]]
     
-Update the %s file to make sure all fun-apps translations are
+Update the %s files to make sure all fun-apps translations are
 up-to-date.
 
 You may also pass the 'all' app name to gather messages for all applications.
@@ -40,9 +39,8 @@ following command in the app folder to make sure the new translations are
 properly compiled:
 
     django-admin.py compilemessages -l <locale>
-""" % (
-    PATH_FUN_DJANGOPO
-)
+""" % PATH_FUN_PO
+
     option_list = BaseCommand.option_list + (
         optparse.make_option('-l', '--locale',
             action="store",
@@ -71,6 +69,16 @@ class MessageMaker(object):
         self.stdout = stdout or sys.stdout
         self.is_verbose = is_verbose
         self.run_compile = run_compile
+        self._edx_catalogs = {}
+
+    def edx_catalog(self, locale, domain):
+        key = (locale, domain)
+        if key not in self._edx_catalogs:
+            self._edx_catalogs[key] = read_po_catalog(
+                PATH_EDX_PO % {'locale': locale, 'domain': domain},
+                locale
+            )
+        return self._edx_catalogs[key]
 
     def handle(self, root_paths, locale, run_compile=False):
         for root_path in root_paths:
@@ -91,63 +99,55 @@ class MessageMaker(object):
         self.make_messages(fun_theme_path, locale, run_compile=run_compile)
 
     def make_messages(self, root_path, locale, run_compile=False):
-
-        # Catalog of required translations
-        pot_catalog = make_pot_catalog(root_path)
-
-        # Catalog of fun translations
-        path_fun_djangopo = PATH_FUN_DJANGOPO % {"locale": locale, "root_path": root_path}
-        fun_catalog = read_po_catalog(path_fun_djangopo, locale)
-
-        # Catalog of edx translations
-        # TODO load edx_catalog once for all apps
-        path_edx_djangopo = PATH_EDX_DJANGOPO % {"locale": locale}
-        edx_catalog = read_po_catalog(path_edx_djangopo, locale)
-
-        update_catalog(fun_catalog, edx_catalog, pot_catalog)
-
-        self.stdout.write("Updating %s...\n" % path_fun_djangopo)
-        if self.is_verbose:
-            check_catalog(fun_catalog)
-
-        fix_catalog_properties(fun_catalog)
-        write_po_catalog(fun_catalog, path_fun_djangopo)
+        self.make_domain_messages(root_path, locale, 'django')
+        self.make_domain_messages(root_path, locale, 'djangojs')
 
         if run_compile:
             compile_messages(root_path, locale)
 
-def fix_catalog_properties(catalog):
-    """Set FUn-compliant catalog properties"""
-    catalog.header_comment = ""
-    catalog.project = u"France Université Numérique"
-    catalog.revision_date = datetime.now()
-    try:
-        catalog.version = str(int(catalog.version) + 1)
-    except ValueError:
-        catalog.version = "0"
-    catalog.copyright_holder = u"France Université Numérique"
-    catalog.msgid_bugs_address = "team@openfun.fr"
-    catalog.language_team = "FUN <team@openfun.fr>"
-    catalog.last_translator = catalog.language_team
+    def make_domain_messages(self, root_path, locale, domain):
+        path_fun_po = fun_app_po_path(root_path, locale, domain)
+        if not os.path.exists(path_fun_po):
+            print "Skipping {} because file does not exist".format(path_fun_po)
+            return
 
-def compile_messages(path, locale):
-    from fun.utils.context import cd, setenv
-    import subprocess
-    with cd(path):
-        with setenv("DJANGO_SETTINGS_MODULE", None):
-            subprocess.call(["/edx/app/edxapp/venvs/edxapp/bin/django-admin.py", "compilemessages", "-l", locale])
+        # Catalog of required translations
+        pot_catalog = make_pot_catalog(root_path, domain=domain)
+
+        # Catalog of fun translations
+        fun_catalog = read_po_catalog(path_fun_po, locale)
+
+        # Catalog of edx translations
+        edx_catalog = self.edx_catalog(locale, domain)
+
+        # Merge FUN and edX catalogs
+        update_catalog(fun_catalog, edx_catalog, pot_catalog)
+
+        if self.is_verbose:
+            # Verify catalog
+            check_catalog(fun_catalog)
+
+        # Fix headers
+        fix_catalog_properties(fun_catalog)
+
+        # Save result
+        self.stdout.write("Updating %s...\n" % path_fun_po)
+        write_po_catalog(fun_catalog, path_fun_po)
 
 def app_root_path(app_name):
     return os.path.join(FUN_APPS_ROOT_DIR, app_name)
 
-def make_pot_catalog(root_path):
-    pot_path = os.path.join(tempfile.gettempdir(), "fun-django.pot")
-    cfg_path = os.path.join(FUN_APPS_ROOT_DIR, "babel.cfg")
+def fun_app_po_path(root_path, locale, domain):
+    return PATH_FUN_PO % {"locale": locale, "root_path": root_path, 'domain': domain}
+
+def make_pot_catalog(root_path, domain='django'):
+    pot_path = os.path.join(tempfile.gettempdir(), "fun-{}.pot".format(domain))
+    cfg_path = os.path.join(FUN_APPS_ROOT_DIR, "fun/locale", "babel-{}.cfg".format(domain))
     extract_command_args = [
-            "pybabel", "--quiet", "extract",
-            "-o", pot_path, "-F", cfg_path,
-            "--keyword=pgettext_lazy:1c,2",
-            root_path
+        "pybabel", "--quiet", "extract",
+        "-o", pot_path, "-F", cfg_path,
+        "--keyword=pgettext_lazy:1c,2",
+        root_path
     ]
     BabelCommandLineInterface().run(extract_command_args)
     pot_catalog = read_po_catalog(pot_path, None)
@@ -156,20 +156,6 @@ def make_pot_catalog(root_path):
 def read_po_catalog(path, locale):
     with open(path) as po_file:
         return read_po(po_file, locale=locale)
-
-def check_catalog(catalog):
-    missing_translations = sorted([
-        message.id for message in catalog
-        if not message.string
-    ])
-    if missing_translations:
-        print "{} missing translations:".format(len(missing_translations))
-        for missing_translation in missing_translations:
-            print "    - {}".format(missing_translation)
-
-def write_po_catalog(catalog, path):
-    with open(path, "w") as catalog_file:
-        write_po(catalog_file, catalog, sort_output=True)
 
 def update_catalog(fun_catalog, edx_catalog, pot_catalog):
     """
@@ -210,6 +196,41 @@ def keep_overriden_messages(fun_catalog, edx_catalog):
     for message_id in overriden_message_ids:
         # default=None is actually required here because of a quirkness in how odict.pop works
         fun_catalog[message_id] = fun_catalog.obsolete.pop(message_id, default=None)
+
+def check_catalog(catalog):
+    missing_translations = sorted([
+        message.id for message in catalog
+        if not message.string
+    ])
+    if missing_translations:
+        print "{} missing translations:".format(len(missing_translations))
+        for missing_translation in missing_translations:
+            print "    - {}".format(missing_translation)
+
+def fix_catalog_properties(catalog):
+    """Set FUn-compliant catalog properties"""
+    catalog.header_comment = ""
+    catalog.project = u"France Université Numérique"
+    catalog.revision_date = datetime.now()
+    try:
+        catalog.version = str(int(catalog.version) + 1)
+    except ValueError:
+        catalog.version = "0"
+    catalog.copyright_holder = u"France Université Numérique"
+    catalog.msgid_bugs_address = "team@openfun.fr"
+    catalog.language_team = "FUN <team@openfun.fr>"
+    catalog.last_translator = catalog.language_team
+
+def write_po_catalog(catalog, path):
+    with open(path, "w") as catalog_file:
+        write_po(catalog_file, catalog, sort_output=True)
+
+def compile_messages(path, locale):
+    from fun.utils.context import cd, setenv
+    import subprocess
+    with cd(path):
+        with setenv("DJANGO_SETTINGS_MODULE", None):
+            subprocess.call(["/edx/app/edxapp/venvs/edxapp/bin/django-admin.py", "compilemessages", "-l", locale])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Make messages to translate for FUN")
