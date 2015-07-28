63b82c8b2365c89f47ab4cbf33a5323156da1c7f
Régis Behmo <regis@behmo.com>
2015-07-28 10:52:06 +0200

    Add script to remove write permissions from wiki articles
    This script is a way around a django-wiki bug that prevents child wiki
    articles from inheriting permissions from their parents.
    
    This closes issue #1629.

---
+++ fun/management/commands/remove_wiki_write_permissions.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+
+import wiki.models.urlpath
+import wiki.models.article
+
+from opaque_keys.edx.keys import CourseKey
+from xmodule.modulestore.django import modulestore
+
+
+class Command(BaseCommand):
+
+    help = """Remove write authorizations from all wiki articles from the given
+courses. This should be doable via the wiki settings view, but a bug in
+django-wiki prevents inheriting read/write permission changes.
+"""
+    args = "<course_id_1> <course_id_2>..."
+
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            raise CommandError("Define at least one course_id argument")
+
+        for course_key_string in args:
+            course_key = CourseKey.from_string(course_key_string)
+            course = modulestore().get_course(course_key)
+            slug = course.wiki_slug
+
+            # Note that this requires on request per argument and could be optimised
+            urlpaths = wiki.models.urlpath.URLPath.objects.select_related("article").filter(slug=slug)
+            if not urlpaths:
+                self.stdout.write("---- Wiki article '{}' could not be found\n".format(slug))
+                continue
+            for urlpath in urlpaths:
+                urlpath.article.group_write = False
+                urlpath.article.other_write = False
+                urlpath.article.save()
+                urlpath_descendants = urlpath.article.descendant_objects()
+                (wiki.models.article.Article.objects.filter(urlpath__in=urlpath_descendants)
+                                                    .update(group_write=False, other_write=False))
+                self.stdout.write("++++ Write permissions removed from wiki '{}'\n".format(slug))
