2757cb13f775b4ec38fdde6a1b620fa08b0754b5
Lazybird <go@lazybird.co>
2015-08-03 16:58:58 +0200

    Add university serializer.

--- courses/api.py
+++ courses/api.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.filters import DjangoFilterBackend
 
+from .serializers import CourseSerializer
 from .models import Course
 
 
 class CourseAPIView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CourseSerializer
     paginate_by = 10
     paginate_by_param = 'rpp'
     filter_backends = (DjangoFilterBackend,)
--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
+from universities.serializers import UniversitySerializer
 from .models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    university = UniversitySerializer()
 
     class Meta:
         model = Course
+        fields = (
+            'id',
+            'key',
+            'university',
+            'level',
+            'subjects',
+        )
