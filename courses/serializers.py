625e5854793144d794c8e7e89605ff29f9f92002
Lazybird <go@lazybird.co>
2015-08-04 12:50:01 +0200

    Add serializer for course subjects.

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 
 from universities.serializers import UniversitySerializer
-from .models import Course
+from .models import Course, CourseSubject
+
+
+class CourseSubjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CourseSubject
+        fields = ('id', 'name')
 
 
 class CourseSerializer(serializers.ModelSerializer):
     university = UniversitySerializer()
+    subjects = CourseSubjectSerializer()
 
     class Meta:
         model = Course
