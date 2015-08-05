eef748e3ad7a344b3ec48826a4e11a6312185742
Lazybird <go@lazybird.co>
2015-08-05 18:16:55 +0200

    Change course api base url.

--- courses/routers.py
+++ courses/routers.py
@@ -4,4 +4,4 @@ from rest_framework.routers import DefaultRouter
 class CourseAPIRouter(DefaultRouter):
 
     def get_default_base_name(self, viewset):
-        return 'courses-api'
+        return 'fun-courses-api'
