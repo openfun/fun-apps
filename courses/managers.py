4f180311c437bebc119431fc07e9c1c2ec834b3a
Lazybird <go@lazybird.co>
2015-08-17 17:23:55 +0200

    Display course subjects on course list page.
    * Course subject now using prefech for related objects.
    * Added aphabetical ordering.

--- courses/managers.py
+++ courses/managers.py
@@ -3,6 +3,15 @@ from django.db import models
 
 class CourseSubjectManager(models.Manager):
 
+    def with_related(self):
+        return self.prefetch_related('courses')
+
+    def by_score(self):
+        return self.with_related().order_by('-score')
+
+    def by_name(self):
+        return self.with_related().order_by('name')
+
     def featured(self):
         return self.filter(featured=True)
 
