8a3e1d7eb082406acfbc64555f9c4e3b1ea4afa7
Lazybird <go@lazybird.co>
2015-09-01 11:44:38 +0200

    Now using a custom class for staticfiles BowerFinder.
    Why are we using a custom BowerFinder class? The finder class provided
    by django-bower uses the standard `django.contrib.staticfiles`.
    There is an issue with that, because edX expects a finder class that
    extends the external `staticfiles` module - not the standard
    module form `contrib`. The custom finder class we are using here
    is a copy of bower's finder that extends the external `staticfiles`
    module.

---
+++ fun/utils/staticfiles_finders.py
@@ -0,0 +1,28 @@
+import collections
+from staticfiles.finders import FileSystemFinder
+from django.core.files.storage import FileSystemStorage
+from djangobower import conf
+import os
+
+
+class BowerFinder(FileSystemFinder):
+    """Find static files installed with bower"""
+
+    def __init__(self, apps=None, *args, **kwargs):
+        self.locations = [
+            ('', self._get_bower_components_location()),
+        ]
+        self.storages = collections.OrderedDict()
+
+        filesystem_storage = FileSystemStorage(location=self.locations[0][1])
+        filesystem_storage.prefix = self.locations[0][0]
+        self.storages[self.locations[0][1]] = filesystem_storage
+
+    def _get_bower_components_location(self):
+        """Get bower components location"""
+        path = os.path.join(conf.COMPONENTS_ROOT, 'bower_components')
+
+        # for old bower versions:
+        if not os.path.exists(path):
+            path = os.path.join(conf.COMPONENTS_ROOT, 'components')
+        return path
