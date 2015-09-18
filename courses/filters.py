3e78f0ea1d24e9a3e07c019a32859992d1203fb9
Lazybird <go@lazybird.co>
2015-09-18 12:32:35 +0200

    Change the way availability parameters are used.
    * Using "availability=new" instead of "new_only=True".
    * Add doc about availability.

--- courses/filters.py
+++ courses/filters.py
@@ -3,33 +3,26 @@ from django.utils.timezone import now, timedelta
 from rest_framework import filters
 
 
-def is_true(value):
-    return value.lower() in ('true', 'yes', '1', 'y')
-
-
 class CourseFilter(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         university_codes = request.QUERY_PARAMS.getlist('university')
         subject_slugs = request.QUERY_PARAMS.getlist('subject')
         levels = request.QUERY_PARAMS.getlist('level')
-        new_only = request.QUERY_PARAMS.get('new_only', 'false')
-        on_demand_only = request.QUERY_PARAMS.get('on_demand_only', 'false')
-        start_soon_only = request.QUERY_PARAMS.get('start_soon_only', 'false')
-        end_soon_only = request.QUERY_PARAMS.get('end_soon_only', 'false')
+        availability = request.QUERY_PARAMS.getlist('availability')
         if university_codes:
             queryset = queryset.filter(universities__code__in=university_codes)
         if subject_slugs:
             queryset = queryset.filter(subjects__slug__in=subject_slugs)
         if levels:
             queryset = queryset.filter(level__in=levels)
-        if is_true(new_only):
+        if 'new' in availability:
             queryset = queryset.filter(is_new=True)
-        if is_true(on_demand_only):
+        if 'on-demand' in availability:
             queryset = queryset.filter(on_demand=True)
         too_late = now() + timedelta(days=7)  # TODO: Move in settings.
-        if is_true(start_soon_only):
+        if 'start-soon' in availability :
             queryset = queryset.filter(start_date__range=(now(), too_late))
-        if is_true(end_soon_only):
+        if 'end-soon' in availability:
             queryset = queryset.filter(end_date__range=(now(), too_late))
         return queryset
