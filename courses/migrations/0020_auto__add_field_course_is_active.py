9c1f6193c9e1dde7547830b5f61d6ad12d8c6895
Lazybird <go@lazybird.co>
2015-09-21 17:56:33 +0200

    Activate course on field creation and also on first course creation.

--- courses/migrations/0020_auto__add_field_course_is_active.py
+++ courses/migrations/0020_auto__add_field_course_is_active.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'Course.is_active'
         db.add_column('courses_course', 'is_active',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      self.gf('django.db.models.fields.BooleanField')(default=True),
                       keep_default=False)
 
 
