b91c7b212223eeeeaa0be5c8044220a21c5e5ea5
Régis Behmo <regis@behmo.com>
2015-09-28 16:55:27 +0200

    funsite: handle course list with no courses

--- fun/envs/lms/dev.py
+++ fun/envs/lms/dev.py
@@ -28,7 +28,7 @@ FEATURES['ENABLE_PAYMENT_FAKE'] = True
 FEATURES['AUTOMATIC_AUTH_FOR_TESTING'] = True
 
 MAKO_MODULE_DIR = None   # this will prevent Mako to cache generated files
-# To totaly deactivate cache we also have de desactive Edx cache on anonymous views
+# To totaly deactivate cache we also have de deactivate edx cache on anonymous views
 # by commenting line 79 (cache.set(cache_key, response, 60 * 3)) in file common/djangoapps/util/cache.py@cache_if_anonymous
 
 # Our new home page is so shiny and chrome that users must see it more often
