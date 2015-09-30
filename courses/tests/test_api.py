fd735658ab2bde020db002fca643443f79836a54
Lazybird <go@lazybird.co>
2015-09-30 11:47:12 +0200

    Simplify, cleanup course list view.
    * Course list view doesn't need context variable anymore.
    * Remove unused filter form.
    * Update translations.

--- courses/tests/test_api.py
+++ courses/tests/test_api.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.core.urlresolvers import reverse
-
-from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from django.test import TestCase
 
 from fun.tests.utils import skipUnlessLms
 
-from .factories import CourseFactory as FunCourseFactory
-
 
 @skipUnlessLms
-class CourseAPITest(ModuleStoreTestCase):
+class CourseAPITest(TestCase):
 
     def setUp(self):
-        super(CourseAPITest, self).setUp()
         self.api_url = reverse('fun-courses-api-list')
-        FunCourseFactory(key='univ1/001/Course1')
 
     def test_list(self):
         response = self.client.get(self.api_url)
-        self.assertContains(response, 'Course1')
+        self.assertContains(response, 'results')
+
