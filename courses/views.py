0be32572ec672cc1832576a9ec89823be90528f9
Lazybird <go@lazybird.co>
2015-09-30 11:47:12 +0200

    Simplify, cleanup course list view.
    * Course list view doesn't need context variable anymore.
    * Remove unused filter form.
    * Update translations.

--- courses/views.py
+++ courses/views.py
@@ -2,63 +2,21 @@
 
 import datetime
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse
 from django.contrib.syndication.views import Feed
-from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
-from django.utils import timezone, translation
 from django.utils.translation import ugettext_lazy as _
 
 from courseware.courses import get_courses, get_course_about_section
 
 from universities.models import University
 
-from .forms import CourseFilteringForm
 from . import utils as courses_utils
 
-COURSES_BY_PAGE = 24
 
-
-def courses_index(request):
-    courses = [courses_utils.dates_description(course) for course in get_courses(request.user)]
-    form = CourseFilteringForm(request.GET or None)
-    by = request.GET.get('by', COURSES_BY_PAGE)  # override default page size
-
-    if form.is_valid():
-        if form.cleaned_data['university']:
-            university = University.objects.get(code=form.cleaned_data['university'])
-            children = [child.code for child in university.children.all()]
-            courses = [c for c in courses if c.org == form.cleaned_data['university'] or c.org in children]
-        if form.cleaned_data['state']:
-            now = timezone.make_aware(datetime.datetime.utcnow(), timezone.get_current_timezone())
-            if form.cleaned_data['state'] == 'future':
-                courses = [c for c in courses if c.start and c.start > now]
-            elif form.cleaned_data['state'] == 'current':
-                courses = [c for c in courses if c.start and c.start < now and c.end and c.end > now]
-            elif form.cleaned_data['state'] == 'past':
-                courses = [c for c in courses if c.end and c.end < now]
-    elif form.errors:
-        return redirect(courses_index)
-    courses = courses_utils.sort_courses(courses)
-
-    # paginate courses
-    paginator = Paginator(courses, by, orphans=3)
-    page = request.GET.get('page')
-
-    try:
-        courses = paginator.page(page)
-    except PageNotAnInteger:
-        courses = paginator.page(1)
-    except EmptyPage:
-        courses = paginator.page(paginator.num_pages)
-
-    return render(request, 'courses/index.html', {
-        'form': form,
-        'courses': courses,
-        'current_language': translation.get_language(),
-    })
-
+class CourseIndexView(TemplateView):
+    template_name = 'courses/index.html'
 
 
 class CoursesFeed(Feed):
@@ -87,3 +45,6 @@ class CoursesFeed(Feed):
 
     def item_link(self, course):
         return reverse('about_course', args=[course.id.to_deprecated_string()])
+
+
+courses_index = CourseIndexView.as_view()
