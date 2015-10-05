c2a8d167fac2a1dc3038536460a215c5e7662945
Richard Moch <richard.moch@gmail.com>
2015-10-05 15:17:34 +0000

    Course about: responsive teaser

--- fun/utils/__init__.py
+++ fun/utils/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from django.conf import settings
 
 from opaque_keys.edx.keys import CourseKey
@@ -23,3 +25,18 @@ def get_fun_course(course):
     """Return the fun-app.courses.models.Course from edX course key."""
     from courses.models import Course
     return Course.objects.get(key=course.id.to_deprecated_string())
+
+def get_teaser(course, video_id, autoplay=True):
+    """Build the DailyMotion url from the video_id and return the html snippet"""
+    if len(video_id) > 20:
+        # Studio saves in mongo youtube urls with dmcloud id, we have to extract dmclouid to build correct url
+        try:
+            dm_id = re.compile(r'/([\d\w]+)\?').search(video_id).groups()[0]
+        except AttributeError:
+            dm_id = ''
+    else:
+        # FUN v1 did the stuff right
+        dm_id = video_id
+    html = '<iframe frameborder="0" scrolling="no" allowfullscreen="" src="//www.dailymotion.com/embed/video/%s/?autoplay=%d"></iframe>' % (
+            dm_id, int(autoplay))
+    return html
