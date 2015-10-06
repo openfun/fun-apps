136de3b2d06361718ce906a3d3c4b212b766992a
Lazybird <go@lazybird.co>
2015-10-06 16:37:13 +0200

    Universities: Remove parent field. Add a way to flag them as obsolete.
    * Add unique together for course-university relation.
    * Add data migration to associate parent universities to course.
    * Add a field to flag obsolete universities.
    * Remove parent field to universities.
    * Add a way to only select universities that are not obsolete.

--- universities/managers.py
+++ universities/managers.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from fun.utils.managers import ChainableManager
 
-class UniversityManager(models.Manager):
+
+class UniversityQuerySet(models.query.QuerySet):
+
+    def active(self):
+        return self.filter(is_obsolete=False)
 
     def with_related(self):
         queryset = self.prefetch_related('courses')
@@ -9,10 +14,14 @@ class UniversityManager(models.Manager):
         return queryset
 
     def by_score(self):
-        return self.with_related().order_by('-score', 'name')
+        return self.active().with_related().order_by('-score', 'name')
 
     def by_name(self):
-        return self.with_related().order_by('name')
+        return self.active().with_related().order_by('name')
 
     def have_page(self):
-        return self.filter(detail_page_enabled=True)
+        return self.active().filter(detail_page_enabled=True)
+
+
+class UniversityManager(ChainableManager):
+    queryset_class = UniversityQuerySet
