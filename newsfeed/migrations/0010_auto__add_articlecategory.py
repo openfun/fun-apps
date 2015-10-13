24ea230f6c6c989f720f11382ff47803aa666cc1
Lazybird <go@lazybird.co>
2015-10-13 15:06:48 +0200

    Update newsfeed backend for fun v3 functionalities.
    * Add article category model.
    * Add support for related articles.
    * Add migration for lead paragraph.
    * Add support for related courses.
    * Add support for article links.
    * Add support for event date.

---
+++ newsfeed/migrations/0010_auto__add_articlecategory.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ArticleCategory'
+        db.create_table('newsfeed_articlecategory', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('order', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
+        ))
+        db.send_create_signal('newsfeed', ['ArticleCategory'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ArticleCategory'
+        db.delete_table('newsfeed_articlecategory')
+
+
+    models = {
+        'newsfeed.article': {
+            'Meta': {'ordering': "['order', '-created_at']", 'object_name': 'Article'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'edited_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "'fr'", 'max_length': '8'}),
+            'microsite': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '128', 'blank': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'newsfeed.articlecategory': {
+            'Meta': {'ordering': "('order', 'id')", 'object_name': 'ArticleCategory'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        'newsfeed.featuredsection': {
+            'Meta': {'object_name': 'FeaturedSection'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'a_+'", 'null': 'True', 'to': "orm['newsfeed.Article']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['newsfeed']
