9a442a0d334624b564c459e76e5421836dabee9c
Régis Behmo <regis@behmo.com>
2015-10-20 17:46:44 +0200

    news: test landing page queries

--- funsite/tests/test_views.py
+++ funsite/tests/test_views.py
@@ -1,14 +1,32 @@
 import random
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from fun.tests.utils import skipUnlessLms
+import newsfeed.tests.factories as newsfactories
 
 
+@skipUnlessLms
 class TestHomepage(TestCase):
+
+    ROOT_PAGE_NUM_QUERIES = 7
 
     def setUp(self):
         random.seed(0)
 
+    def get_root_page(self):
+        return self.client.get(reverse("root"))
+
     def test_no_course(self):
-        self.client.get("")
+        with self.assertNumQueries(self.ROOT_PAGE_NUM_QUERIES):
+            self.get_root_page()
+
+    def test_news_do_not_require_additional_queries(self):
+        category = newsfactories.ArticleCategoryFactory.create()
+        newsfactories.ArticleFactory.create(category=category)
+        newsfactories.ArticleFactory.create(category=category)
+        newsfactories.ArticleFactory.create(category=category)
+
+        with self.assertNumQueries(self.ROOT_PAGE_NUM_QUERIES):
+            self.get_root_page()
