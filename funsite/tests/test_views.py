306f0820f516f98f3ae590de879eb7cbd156cd58
Régis Behmo <regis@behmo.com>
2015-10-21 14:30:43 +0200

    home page: do not display inappropriate news
    Unpublished news and news from a different microsite should not be
    displayed on the home page.
    
    This closes issue #1999.

--- funsite/tests/test_views.py
+++ funsite/tests/test_views.py
@@ -3,7 +3,8 @@ import random
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from fun.tests.utils import skipUnlessLms
+from backoffice.tests.test_microsites import FAKE_MICROSITE1, FAKE_MICROSITE2
+from fun.tests.utils import skipUnlessLms, setMicrositeTestSettings
 import newsfeed.tests.factories as newsfactories
 
 
@@ -26,3 +27,17 @@ class TestHomepage(TestCase):
         newsfactories.ArticleFactory.create(category=category)
 
         self.get_root_page()
+
+    def test_unpublished_news_are_not_displayed(self):
+        article = newsfactories.ArticleFactory.create(published=False)
+        response = self.get_root_page()
+        self.assertFalse(str(article.title) in response.content)
+
+    @setMicrositeTestSettings(FAKE_MICROSITE2)
+    def test_news_from_different_microsite_are_not_displayed(self):
+        article = newsfactories.ArticleFactory.create(
+            microsite=FAKE_MICROSITE1["SITE_NAME"],
+            published=True
+        )
+        response = self.get_root_page()
+        self.assertFalse(str(article.title) in response.content)
