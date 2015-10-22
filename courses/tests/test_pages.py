fe29a09f01e3711c8f37575e5145b08abfacc3a6
Lazybird <go@lazybird.co>
2015-10-22 18:08:18 +0200

    Add course API tests.
    * Add test for university filtering.
    * Add test for subjects filtering.
    * Add test for level filtering.
    * Add tests for start/end soon filtering.

---
+++ courses/tests/test_pages.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from fun.tests.utils import skipUnlessLms
+
+
+@skipUnlessLms
+class CourseListTest(TestCase):
+
+    def test_course_list_page_loads(self):
+        url = reverse('fun-courses-index')
+        response = self.client.get(url)
+        self.assertContains(response, 'courses')
