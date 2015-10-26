43e4f41324ab20d8deafdc12320325e933a225b4
Lazybird <go@lazybird.co>
2015-10-26 11:04:30 +0100

    Refactor model tests: change some asserts and some variable names.

--- courses/tests/test_models.py
+++ courses/tests/test_models.py
@@ -12,26 +12,43 @@ from . import factories
 @skipUnlessLms
 class TestCourseSubject(TestCase):
 
-    def test_annotate_with_public_courses(self):
-        course1 = factories.CourseFactory.create(key="key1", is_active=False, show_in_catalog=False)
-        course2 = factories.CourseFactory.create(key="key2", is_active=True, show_in_catalog=False)
-        course3 = factories.CourseFactory.create(key="key3", is_active=True, show_in_catalog=True)
-        course4 = factories.CourseFactory.create(key="key4", is_active=True, show_in_catalog=True)
+    def test_ability_to_count_public_courses_for_course_subjects(self):
+        course_not_active = factories.CourseFactory.create(
+            key="key1", is_active=False, show_in_catalog=False
+        )
+        course_not_in_catalog = factories.CourseFactory.create(
+            key="key2", is_active=True, show_in_catalog=False
+        )
+        course_active_1 = factories.CourseFactory.create(
+            key="key3", is_active=True, show_in_catalog=True
+        )
+        course_active_2 = factories.CourseFactory.create(
+            key="key4", is_active=True, show_in_catalog=True
+        )
 
-        subject1 = factories.CourseSubjectFactory.create(score=1, slug="slug1")
-        subject2 = factories.CourseSubjectFactory.create(score=2, slug="slug2")
-        subject3 = factories.CourseSubjectFactory.create(score=3, slug="slug3")
-        subject4 = factories.CourseSubjectFactory.create(score=4, slug="slug4")
+        subject_not_active = factories.CourseSubjectFactory.create(
+            score=1, slug="not-active"
+        )
+        subject_not_in_catalog = factories.CourseSubjectFactory.create(
+            score=2, slug="not-in-catalog"
+        )
+        subject_active_1 = factories.CourseSubjectFactory.create(
+            score=3, slug="active-1"
+        )
+        subject_active_2 = factories.CourseSubjectFactory.create(
+            score=4, slug="active-2"
+        )
 
-        course1.subjects.add(subject1)
-        course2.subjects.add(subject2)
-        course3.subjects.add(subject3)
-        course4.subjects.add(subject4)
+        course_not_active.subjects.add(subject_not_active)
+        course_not_in_catalog.subjects.add(subject_not_in_catalog)
+        course_active_1.subjects.add(subject_active_1)
+        course_active_2.subjects.add(subject_active_2)
 
         subjects = managers.annotate_with_public_courses(models.CourseSubject.objects.all())
 
-        self.assertEqual(2, len(subjects))
-        self.assertEqual(subject4.pk, subjects[0].pk)
-        self.assertEqual(subject3.pk, subjects[1].pk)
-        self.assertEqual(1, subjects[0].public_courses_count)
-        self.assertEqual(1, subjects[1].public_courses_count)
+        self.assertNotIn(subject_not_active, subjects)
+        self.assertNotIn(subject_not_in_catalog, subjects)
+        self.assertIn(subject_active_1, subjects)
+        self.assertIn(subject_active_2, subjects)
+        self.assertEqual(1, subjects.get(slug='active-1').public_courses_count)
+        self.assertEqual(1, subjects.get(slug='active-2').public_courses_count)
