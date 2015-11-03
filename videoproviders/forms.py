f156ba3e7c60bdb70d524ea79ceb428688256336
Régis Behmo <regis@behmo.com>
2015-11-03 12:08:10 +0100

    video upload: set default subtitle language to French
    This closes issue #1879.

--- videoproviders/forms.py
+++ videoproviders/forms.py
@@ -26,7 +26,7 @@ class SubtitleForm(VideoForm):
 
     # Other formats supported by Dailymotion are 'STL', 'TT', but we don't support them for now.
     uploaded_file = forms.FileField(label=_("Subtitle file (SRT files only)"))
-    language = forms.ChoiceField(label=_("Subtitle language"), choices=SUPPORTED_LANGUAGES)
+    language = forms.ChoiceField(label=_("Subtitle language"), choices=SUPPORTED_LANGUAGES, initial='fr')
 
 
 class ThumbnailForm(VideoForm):
