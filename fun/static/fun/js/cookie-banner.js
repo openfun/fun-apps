3edcfffc7b09751cd8e52a4594343ee79aaac61a
Régis Behmo <regis@behmo.com>
2015-11-04 12:35:03 +0100

    CNIL cookie: expire after 6 months instead of 24h.

--- fun/static/fun/js/cookie-banner.js
+++ fun/static/fun/js/cookie-banner.js
@@ -1,18 +1,22 @@
 var cookieName = 'acceptCookieFun';
 var cookieValue = "on";
 var cookieDuration = 365;
-var cookieBannerHtml = "<h2>france-universite-numerique-mooc.fr utilise des cookies de mesure d'audience dans le but d\'am&eacute;liorer le service.</h2>\
-    En naviguant sur notre site vous acceptez l\'installation et l\'utilisation des cookies sur votre ordinateur.\
-    <a href=\"#\">&gt; En savoir plus</a>\
-    <a href=\"#\" class=\"cookie-banner-button\">J'accepte</a>";
+var cookieBannerHtml = "<h2>france-universite-numerique-mooc.fr utilise des cookies de mesure d'audience dans le but d\'am&eacute;liorer le service.</h2>" +
+    "En naviguant sur notre site vous acceptez l\'installation et l\'utilisation des cookies sur votre ordinateur." +
+    "<a href=\"#\">&gt; En savoir plus</a>" +
+    "<a href=\"#\" class=\"cookie-banner-button\">J'accepte</a>";
 
 function checkCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
     }
     return null;
 }
@@ -22,7 +26,8 @@ function removeDiv(event) {
     var element = document.getElementById('cookie-banner');
     element.parentNode.removeChild(element);
     var date = new Date();
-    date.setTime(date.getTime() + (window.cookieDuration*24*60*60*1000));
+    // Cookie expires after six months
+    date.setTime(date.getTime() + (window.cookieDuration*180*24*60*60*1000));
     var expires = "; expires=" + date.toGMTString();
     document.cookie = window.cookieName + "=" + window.cookieValue + expires + "; path=/";
 }
@@ -44,8 +49,7 @@ function animateBanner(event) {
         clearInterval(id);
       }
     }
-    var id = setInterval(frame, 4) 
-
+    var id = setInterval(frame, 4);
 }
 
 function manageCookieBanner(event) {
