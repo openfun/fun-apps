46fbcbd3aaff2f9bc1ce8f045fd521b618ffb16f
jpaille <julien.paille75@gmail.com>
2015-11-06 11:29:39 +0000

    Add register_info to static pages.

--- funsite/urls.py
+++ funsite/urls.py
@@ -3,8 +3,11 @@
 from django.conf.urls import patterns, url
 
 
-static_pages = ['about', 'honor', 'legal', 'privacy', 'tos']
+static_pages = ['about', 'honor', 'legal', 'privacy', 'tos', 'register_info']
 
-urls = [url(r'^(?P<page>({}))/?$'.format(name), 'render_static_template', name=name) for name in static_pages]
+urls = [url(r'^(?P<page>({}))/?$'.format(name),
+             'render_static_template',
+             name=name)
+        for name in static_pages]
 
 urlpatterns = patterns('funsite.views', *urls)
