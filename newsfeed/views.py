9aeb52e01c1ffd1b4e4ae6d3b642587107bc8150
Régis Behmo <regis@behmo.com>
2015-11-10 11:00:05 +0100

    newsfeed: simplify news list
    Transition from class-based views to regular views. In the process, make
    sure that all article lists passed to templates are arrays, and not
    querysets.

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -17,45 +17,31 @@ from . import models
 
 def top_news(count=5):
     """Return Top count news if available or fill result list with None for further boolean evaluation."""
-    articles = ArticleListView().get_articles(with_featured_section=True)
+    articles = get_articles()[:count]
     return [articles[idx] if len(articles) > idx else None for idx in range(count)]
 
-class StaffOnlyView(object):
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user or not request.user.is_staff:
-            raise django.http.Http404
-        return super(StaffOnlyView, self).dispatch(request, *args, **kwargs)
+def article_list(request):
+    # We exclude the article that's selected in the featured section.
+    return render_articles(get_articles().filter(featured_section__isnull=True))
 
+@staff_required
+def article_list_preview(request, slug):
+    return render_articles(filter_queryset_for_site(models.Article.objects.published_or(slug=slug)))
+
+def render_articles(articles_queryset):
+    return render_to_response('newsfeed/article/list.html', {
+        'articles': list(articles_queryset)
+    })
+
+def get_articles():
+    # Note: We might want to filter on language and limit the queryset to
+    # the first n results in the future (see the .featured() method).
+    return filter_queryset_for_site(models.Article.objects.viewable())
 
 def filter_queryset_for_site(queryset):
     if settings.FEATURES['USE_MICROSITES']:
         queryset = queryset.filter(microsite=microsite.get_value('SITE_NAME'))
     return queryset
-
-class ArticleListView(mako.MakoTemplateMixin, ListView):
-    template_name = 'newsfeed/article/list.html'
-    context_object_name = 'articles'
-
-    def get_queryset(self):
-        return self.get_articles(with_featured_section=False)
-
-    def get_articles(self, with_featured_section=True):
-        # Note: We might want to filter on language and limit the queryset to
-        # the first n results in the future (see the .featured() method).
-        queryset = filter_queryset_for_site(models.Article.objects.viewable())
-
-        # We exclude the article that's selected in the featured section.
-        if not with_featured_section:
-            queryset = queryset.filter(featured_section__isnull=True)
-
-        return queryset
-article_list = ArticleListView.as_view()
-
-
-class ArticleListPreviewView(StaffOnlyView, ArticleListView):
-    def get_queryset(self):
-        return filter_queryset_for_site(models.Article.objects.published_or(slug=self.kwargs['slug']))
-article_list_preview = ArticleListPreviewView.as_view()
 
 
 def article_detail(request, slug):
