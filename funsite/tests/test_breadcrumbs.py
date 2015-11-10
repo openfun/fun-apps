4a817889fb0220380d136030b6a51b50ca84f1e9
Richard Moch <richard.moch@gmail.com>
2015-11-10 15:55:13 +0000

    #1861 Breadcrumbs

---
+++ funsite/tests/test_breadcrumbs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils.translation import pgettext, ugettext as _
+
+from fun.tests.utils import skipUnlessLms
+
+from ..utils import breadcrumbs
+
+
+@skipUnlessLms
+class TestBreadcrumbs(TestCase):
+
+    def test_static_pages(self):
+        url = '/privacy'
+        breadcrumbs_list = breadcrumbs(url, u"Privacy")
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), ('#', u"Privacy")],)
+
+        url = '/contact'
+        breadcrumbs_list = breadcrumbs(url, u"Contact")
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), ('#', u"Contact")],)
+
+    def test_course_list(self):
+        url = '/cours/'
+        breadcrumbs_list = breadcrumbs(url, _("Courses"))
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), ('#', _("Courses"))],)
+
+    def test_course_about_page(self):
+        url = '/courses/fun/001/now/about'
+        breadcrumbs_list = breadcrumbs(url, u"Test course")
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), (reverse('fun-courses:index'), _("All courses")), ('#', u"Test course")],)
+
+    def test_news_detail_page(self):
+        url = '/news/test-news'
+        breadcrumbs_list = breadcrumbs(url, u"Test news")
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), (reverse('newsfeed-landing'), pgettext("fun-news", "News")), ('#', u"Test news")],)
+
+    def test_university_page(self):
+        url = '/universities/fun-paris-V'
+        breadcrumbs_list = breadcrumbs(url, u"FUN Paris V")
+        self.assertEqual(breadcrumbs_list,
+                [('/', _("Home")), (reverse('universities-landing'), _(u"Universities")), ('#', u"FUN Paris V")],)
---
+++ funsite/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+
+from django.utils.translation import pgettext, ugettext as _
+
+BreadcrumbsItem = namedtuple('BreadcrumbsItem', ['path', 'name'])
+
+
+def breadcrumbs(url, current_page):
+    """
+    Returns a BreadcrumbsItem list of pairs (path, i18ned name) of length from 2 to 3
+    as the breadcrumb for current url
+    The tricky part is the replacement of edX's courses page by our.
+    When length is 3. The first item is the home page.
+            The last item is the current page. The middle item, if present, points to the parent
+            page of the current page.
+    See tests.test_breadcrumbs to understand awaited behaviour
+    """
+
+    mapping = {
+        'contact': _("Contact"),
+        'courses': _("All courses"),    # edX page
+        'cours': _("All courses"),      # FUN page
+        'news': pgettext("fun-news", "News"),
+        'universities': _("Universities"),
+    }
+    result = [BreadcrumbsItem(path='/', name=_(u"Home"))]
+
+    items = url.split('/')[1:]  # split and remove first backslash
+    if len(items) > 1 and items[1]:
+        if items[0] == 'courses':
+            items[0] = 'cours'
+        result.append(BreadcrumbsItem(path='/%s/' % items[0], name=mapping[items[0]]))
+
+    result.append(BreadcrumbsItem(path='#', name=current_page))
+    return result
+
