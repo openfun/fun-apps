8e782dcd4c346597e1cdcf2ac7667bc49106c5c8
Régis Behmo <regis@behmo.com>
2015-11-16 17:03:22 +0100

    lms: fix theme url on landing page
    Theme url ended with an extra "/". This was fixed by moving the relevant
    code to courses_api, which is lms-only.
    
    This closes issue #2103.

--- courses_api/urls.py
+++ courses_api/urls.py
@@ -9,6 +9,7 @@ from .api import CourseAPIView
 
 urlpatterns = patterns('',
     url(r'^$', courses_index, name='index'),
+    url(r'^#filter/subject/(?P<subject>.+)$', courses_index, name='filter'),
     url(r'^feed/$', CoursesFeed(), name='feed'),
 )
 
