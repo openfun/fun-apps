9046275555f8c8862891d317fa841d3d01cc62a4
Lazybird <go@lazybird.co>
2015-11-30 11:39:21 +0100

    Add enrollment start/end date to API response

--- courses_api/tests/test_api.py
+++ courses_api/tests/test_api.py
@@ -225,6 +225,7 @@ class CourseAPITest(TestCase):
         filter_data = {'availability': 'enrollment-ends-soon'}
         response = self.client.get(self.api_url, filter_data)
         data = json.loads(response.content)
+
         self.assertEqual(1, len(data['results']))
         self.assertEqual(self.active_1.title, data['results'][0]['title'])
 
