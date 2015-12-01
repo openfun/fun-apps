b3b67cef0619cc8a8a4881157f8bab0eb0031da2
Lazybird <go@lazybird.co>
2015-12-01 11:10:39 +0100

    Rename get token URL entry

--- fun_api/urls.py
+++ fun_api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('fun_api.views',
-    url(r'^$', 'get_token', name='fun-get-token'),
+    url(r'^$', 'get_token', name='get-token'),
 )
