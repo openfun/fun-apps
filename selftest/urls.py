0dcaf97f33d97ce6aa1641903a975b1afca8eee4
jpaille <julien.paille75@gmail.com>
2015-12-14 10:06:16 +0000

    Add a celery error test in selftest application.
    New button to trigger a 500 error in a celery task.
    This can help us knowing if we correctly log workers error in sentry.

--- selftest/urls.py
+++ selftest/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('selftest.views',
     url(r'^$', 'selftest_index', name='self-test-index'),
     url(r'^page_not_found/$', 'page_not_found', name='self-test-404'),
     url(r'^server_error/$', 'server_error', name='self-test-500'),
+    url(r'^worker_error/$', 'worker_error', name='worker-error')
 )
