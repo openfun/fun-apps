8fa76fc8108ce71a0366689fc4917e228b3f3d58
Lazybird <go@lazybird.co>
2015-12-15 18:10:36 +0100

    Add enrollment start/end date to API response

--- courses_api/serializers.py
+++ courses_api/serializers.py
@@ -48,6 +48,8 @@ class CourseSerializer(serializers.ModelSerializer):
             'start_date_display',
             'end_date_display',
             'end_date',
+            'enrollment_start_date',
+            'enrollment_end_date',
             'course_started',
             'course_ended',
             'enrollment_ended',
