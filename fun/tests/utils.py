9d7568d146c2f4d40af0183d196508f9725f0b50
Richard Moch <richard.moch@gmail.com>
2015-12-22 16:06:39 +0000

    Add RSS feed for published news
    Also factorize tests for Courses and News feed
    
    closes #1159

--- fun/tests/utils.py
+++ fun/tests/utils.py
@@ -2,8 +2,14 @@
 
 import mock
 import unittest
+import xmltodict
+
+from bs4 import BeautifulSoup
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
 import fun.utils
 
 
@@ -12,6 +18,40 @@ def skipUnlessCms(func):
 
 def skipUnlessLms(func):
     return unittest.skipUnless(fun.utils.is_lms_running(), 'Test only valid in lms')(func)
+
+
+class RSSDeclarationMixin():
+    """This mixin is used by both Courses and News RSS feed.
+    TestCase has to be carefully set up because feeds are reversely sorted.
+    """
+    def get_parsed_feed(self):
+        response = self.client.get(self.url)
+        return xmltodict.parse(response.content)['rss']
+
+    def test_HTML_HEAD_declaration(self):
+        """Test if self.url is well declared in HTML header as RSS feed."""
+        url = reverse('root')
+        response = self.client.get(url)
+        soup = BeautifulSoup(response.content)
+        declarations = soup.select('head > link[type="application/rss+xml"]')  # get all rss declarations
+        self.assertTrue(any([declaration.attrs['href'] == self.url for declaration in declarations]))
+
+    def test_feed(self):
+        """Test view return status code 200."""
+        self.assertEqual(200, self.client.get(self.url).status_code)
+
+    def test_feed_for_unpublished_item(self):
+        """Test item1 which should not be public is not in feed."""
+        feed = self.get_parsed_feed()
+        self.assertTrue('item1' not in [i['title'] for i in feed['channel']['item']])
+
+    def test_feed_description_structure(self):
+        """Test HTML description structure, see feed.html templates which chould have common structures for item title."""
+        feed = self.get_parsed_feed()
+        description = dict(dict(dict(feed)['channel'])['item'][0])['description']  # get the HTML description of the first item
+        soup = BeautifulSoup(description)
+        self.assertEqual(self.item2.title, soup.select('html h1 a')[0].text)
+
 
 ### Microsite test settings
 
@@ -34,3 +74,5 @@ def setMicrositeTestSettings(microsite_settings=None):
         test_func = mock.patch("microsite_configuration.microsite.get_value", fake_settings.get)(test_func)
         return mock.patch.dict(settings.FEATURES, {'USE_MICROSITES' : True, 'USE_CUSTOM_THEME' : False})(test_func)
     return wrapper
+
+
---
+++ newsfeed/tests/test_feed.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import xmltodict
+
+from bs4 import BeautifulSoup
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils.translation import ugettext_lazy as _
+
+from fun.tests.utils import skipUnlessLms, RSSDeclarationMixin
+
+from .factories import ArticleCategoryFactory, ArticleFactory
+
+
+class TestNewsFeed(TestCase, RSSDeclarationMixin):
+    def setUp(self):
+        self.url = reverse('newsfeed-rss')
+        category = ArticleCategoryFactory()
+        date = datetime.datetime(2015, 1, 1)
+        self.item1 = ArticleFactory(published=False, title=u"Title 1", category=category, created_at=date)
+        self.item2 = ArticleFactory(published=True, title=u"Title 2", category=category, created_at=date + datetime.timedelta(days=15))
+        self.item3 = ArticleFactory(published=True, title=u"Title 3", created_at=date)
+
+    def test_feed_xml_structure(self):
+        feed = self.get_parsed_feed()
+        self.assertEqual(_(u"Fun latest published news"), feed['channel']['title'])
+        item2 = dict(feed['channel']['item'][0])  # article 2 should be first
+        item3 = dict(feed['channel']['item'][1])
+        self.assertEqual('Title 2', item2['title'])
+        self.assertEqual('Title 3', item3['title'])
