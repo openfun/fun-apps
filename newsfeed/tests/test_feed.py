bef7e88ddf0d8b1e152722e5521b852fd819fcb1
Régis Behmo <regis@behmo.com>
2016-02-01 15:07:58 +0100

    Skip news feed tests in cms
    This should fix currently breaking unit tests.

--- newsfeed/tests/test_feed.py
+++ newsfeed/tests/test_feed.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-import xmltodict
-
-from bs4 import BeautifulSoup
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
@@ -14,13 +11,15 @@ from fun.tests.utils import skipUnlessLms, RSSDeclarationMixin
 from .factories import ArticleCategoryFactory, ArticleFactory
 
 
+@skipUnlessLms
 class TestNewsFeed(TestCase, RSSDeclarationMixin):
     def setUp(self):
         self.url = reverse('newsfeed-rss')
         category = ArticleCategoryFactory()
         date = datetime.datetime(2015, 1, 1)
         self.item1 = ArticleFactory(published=False, title=u"Title 1", category=category, created_at=date)
-        self.item2 = ArticleFactory(published=True, title=u"Title 2", category=category, created_at=date + datetime.timedelta(days=15))
+        self.item2 = ArticleFactory(published=True, title=u"Title 2", category=category,
+                                    created_at=date + datetime.timedelta(days=15))
         self.item3 = ArticleFactory(published=True, title=u"Title 3", created_at=date)
 
     def test_feed_xml_structure(self):
