c646ce5ee15da056cdf27bcc2da21481c39b9be5
Richard Moch <richard.moch@gmail.com>
2016-02-23 17:01:19 +0000

    wip

--- payment/urls.py
+++ payment/urls.py
@@ -3,10 +3,9 @@
 from django.conf.urls import include, url, patterns
 
 urlpatterns = patterns('payment.views',
-    url(r'^fake-payment-page/$', 'fake_payment_page', name="payment-fake"),
-
-    url(r'^postpay-success/$', 'postpay_success', name="payment-success"),
-    url(r'^postpay-cancel/$', 'postpay_cancel', name="payment-cancel"),
+    url(r'^success/$', 'paybox_success', name="payment-success"),
+    url(r'^notification/$', 'paybox_notification', name="payment-notification"),
+    url(r'^cancel/$', 'paybox_cancel', name="payment-cancel"),
 )
 
 
--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,8 @@
 
 import logging
 
+import requests
+
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -14,23 +16,10 @@ from edxmako.shortcuts import render_to_response
 
 from commerce import ecommerce_api_client
 
+ECOMMERCE_NOTIFICATION_URL = 'http://localhost:8002/payment/paybox/notify/'
 
 @csrf_exempt
-def fake_payment_page(request):
-    
-    return render_to_response('payment/fake.html', {
-        'fields': request.POST,
-        })
-
-
-@csrf_exempt
-def postpay_success(request):
-    import ipdb; ipdb.set_trace()
-
-    order = request.GET['order']
-
-    order = ecommerce_api_client(request.user).baskets(order).order.get()
-
+def paybox_success(request):
 
 
     return render_to_response('payment/return.html', {
@@ -38,7 +27,19 @@ def postpay_success(request):
         })
 
 @csrf_exempt
-def postpay_cancel(request):
+def paybox_cancel(request):
 
     return render_to_response('payment/cancel.html', {
         })
+
+
+@csrf_exempt
+def paybox_notification(request):
+    """This view will proxy Paybox notifications to ecommerce service,
+    to avoid internet exposition of bank VM."""
+
+    
+
+    __ = requests.post(ECOMMERCE_NOTIFICATION_URL, request.POST)
+
+    return HttpResponse()  # Paybox do not expects any response
