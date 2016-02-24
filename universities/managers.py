d64356d8fb5126beff76b1f94c22a316120d89ca
Régis Behmo <regis@behmo.com>
2016-02-24 12:22:47 +0100

    Simplify universities queryset methods
    This refactor should not introduce any change.

--- universities/managers.py
+++ universities/managers.py
@@ -9,18 +9,15 @@ class UniversityQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(is_obsolete=False)
 
+    def detail_page_enabled(self):
+        return self.filter(detail_page_enabled=True)
+
     def with_related(self):
         queryset = self.prefetch_related('courses')
         return queryset
 
     def by_score(self):
         return self.order_by('-score', 'name')
-
-    def by_name(self):
-        return self.active().with_related().order_by('name')
-
-    def have_page(self):
-        return self.active().filter(detail_page_enabled=True)
 
 
 class UniversityManager(ChainableManager):
