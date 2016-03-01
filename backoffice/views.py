90375428bfa16f38314d8784290c388cd5851d6f
Richard Moch <richard.moch@gmail.com>
2016-03-01 09:13:12 +0000

    Backoffice: Many missing i18n

--- backoffice/views.py
+++ backoffice/views.py
@@ -181,7 +181,8 @@ def resend_activation_email(request, user):
     logger.warning(u"Activation email has been resent to user %s at addresse: %s", user.username, user.email)
     messages.success(
         request,
-        _(u"Activation email has been resent to user %s at addresse: %s") % (user.username, user.email)
+        _(u"Activation email has been resent to user {username} at addresse: {email}".format(
+                username=user.username, email=user.email))
     )
 
 
