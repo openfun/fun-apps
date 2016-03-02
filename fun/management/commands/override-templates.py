ce6a0755f3a7ad3aea4d448ca80809834cfcc0f6
Régis Behmo <regis@behmo.com>
2016-03-02 11:05:23 +0100

    Stop overriding forum contributors membership template
    This template is now overridden directly in edx-platform.

--- fun/management/commands/override-templates.py
+++ fun/management/commands/override-templates.py
@@ -10,10 +10,6 @@ from fun.utils.context import cd
 # List of edx-platform/fun-apps tuples that indicate relative paths of templates to override.
 OVERRIDDEN_TEMPLATES = [
     (
-        "lms/templates/instructor/instructor_dashboard_2/membership.html",
-        "forum_contributors/templates/instructor/instructor_dashboard_2/membership.html"
-    ),
-    (
         "lms/templates/discussion/_js_head_dependencies.html",
         "forum_contributors/templates/discussion/_js_head_dependencies.html"
     ),
