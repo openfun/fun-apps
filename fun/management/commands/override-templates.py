3836339c1121ba81db67ad67328098ae892c8354
Régis Behmo <regis@behmo.com>
2016-03-02 11:15:10 +0100

    Stop overriding _underscore_templates.html
    This template is now overridden directly in our fork of edx-platform.

--- fun/management/commands/override-templates.py
+++ fun/management/commands/override-templates.py
@@ -10,10 +10,6 @@ from fun.utils.context import cd
 # List of edx-platform/fun-apps tuples that indicate relative paths of templates to override.
 OVERRIDDEN_TEMPLATES = [
     (
-        "lms/templates/discussion/_underscore_templates.html",
-        "forum_contributors/templates/discussion/_underscore_templates.html"
-    ),
-    (
         "lms/templates/discussion/index.html",
         "forum_contributors/templates/discussion/index.html"
     ),
