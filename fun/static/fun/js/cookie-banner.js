5257a97447bea1b71bd0278191bd40be3712be54
Régis Behmo <regis@behmo.com>
2016-03-08 15:41:04 +0100

    Move common js code to header.js
    Some ad-blockers block the cookie-banner.js script from being loaded. As
    a consequence, the checkCookie function was no more available to
    header.js. Here, we fix this issue by moving the checkCookie function to
    header.js.
    
    This closes issue #2541.

--- fun/static/fun/js/cookie-banner.js
+++ fun/static/fun/js/cookie-banner.js
@@ -1,20 +1,3 @@
-// Note: this function should probably be in a separate script file, as it is
-// used elsewhere.
-function checkCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1,c.length);
-        }
-        if (c.indexOf(nameEQ) === 0) {
-            return c.substring(nameEQ.length,c.length);
-        }
-    }
-    return null;
-}
-
 (function() {
     var cookieName = 'acceptCookieFun';
     var cookieValue = "on";
