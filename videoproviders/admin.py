c9710e78bf5ba3abeae2fc4982da457a25411664
Richard Moch <richard.moch@gmail.com>
2016-03-23 14:10:42 +0000

    Remove Dailymotion credentials from University model and api code from videoprovider application. Closes #2136

--- videoproviders/admin.py
+++ videoproviders/admin.py
@@ -3,12 +3,6 @@ from django.contrib import admin
 from . import models
 
 
-class DailymotionAuthAdminInline(admin.StackedInline):
-    model = models.DailymotionAuth
-    fields = ("username", "password", "api_key", "api_secret", "access_token", "refresh_token", "expires_at_time",)
-    readonly_fields = ("access_token", "refresh_token", "expires_at_time",)
-
-
 class LibcastAuthAdminInline(admin.StackedInline):
     model = models.LibcastAuth
     fields = ("username", "api_key",)
