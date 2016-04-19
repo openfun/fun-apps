5ed1b21b11165e373d86e90bb4f6193f444fe182
Lazybird <go@lazybird.co>
2016-04-19 16:56:06 +0200

    Change update courses / search index periodic tasks
    - Search index is now done independently - not attached to course meta
      data update anymore.
    - Change frequency for the course update scheduler.

--- courses/tasks.py
+++ courses/tasks.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import datetime
-
 from celery import shared_task
 
 from django.core.management import call_command
@@ -10,11 +8,8 @@ from django.core.management import call_command
 @shared_task
 def update_courses_meta_data(*args, **kwargs):
     '''
-    A task that servers as proxy to the management command.
+    A task that serves as proxy to the management command.
     Can be used for instance when a signal is fired to update
     a single course or to run periodic tasks.
     '''
-
-    now = datetime.datetime.now().isoformat()
     call_command('update_courses', *args, **kwargs)
-    call_command('update_index', start_date=now, *args, **kwargs)
