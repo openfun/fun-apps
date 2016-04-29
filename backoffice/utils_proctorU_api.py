261c2b4c27beebe5994fb7592a55e9e777d8934e
Matthieu Falce <falce.matthieu@gmail.com>
2016-04-29 16:06:46 +0200

    Display the students test results in the course backoffice.
    This closes #2887

--- backoffice/utils_proctorU_api.py
+++ backoffice/utils_proctorU_api.py
@@ -87,10 +87,12 @@ def extract_infos(report):
         "IncidentReport": report["IncidentReport"],
         "Escalated": report["Escalated"],
         "fun_user_url": None,
+        "fun_exam_grade": None,
+        "fun_exam_pass": None,
     }
     return tmp
 
-def get_protectU_students(course_name, course_run):
+def get_protectU_students(course_name, course_run, student_grades):
     data = request_infos()
 
     student_activity = query_api(requests.post,
@@ -103,10 +105,10 @@ def get_protectU_students(course_name, course_run):
     if "error" in filtered_reports:
         return filtered_reports
 
-    return aggregate_reports_per_user(filtered_reports)
+    return aggregate_reports_per_user(filtered_reports, student_grades)
 
 
-def aggregate_reports_per_user(filtered_reports):
+def aggregate_reports_per_user(filtered_reports, student_grades):
     """
     Aggregate the lines present in the API according the user.
     The API returns a line for each events, but we are really interested in the "profile" for each user.
@@ -115,7 +117,8 @@ def aggregate_reports_per_user(filtered_reports):
     duplicates.
 
     :param filtered_reports: iterable with the ProctorU reports filtered with the course of interest
-    :return: dict with the procterU user ID as key and the list of "actions" / reports for this user
+    :param student_grades: dictionary with the student username as key and info about their exam grade in values
+    :return: dict with the proctorU user ID as key and the list of "actions" / reports for this user
     """
     filtered_reports.sort(key=lambda d: d["Student"])
 
@@ -145,6 +148,15 @@ def aggregate_reports_per_user(filtered_reports):
 
         url = reverse("backoffice:user-detail", args=[user.username])
         event_users[id_][0]["fun_user_url"] = url
+
+        try:
+            grade, passed = student_grades[user.username]["grade"], student_grades[user.username]["passed"]
+        except KeyError:
+            mess = "User {} is not in student_grades".format(user.username)
+            logger.info(mess)
+        else:
+            event_users[id_][0]["fun_exam_grade"] = grade
+            event_users[id_][0]["fun_exam_pass"] = passed
     return dict(event_users)
 
 
