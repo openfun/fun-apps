963b437529dce2bc7d34a9021a3cdecd09fc3046
Matthieu Falce <falce.matthieu@gmail.com>
2016-05-02 13:42:08 +0200

    Display ProcterU student in backoffice
     * Bugfix for production case
     * Better wording for error / warning messages
     * Adds translation for messages in the backoffice
    This closes #2887

--- backoffice/utils_proctorU_api.py
+++ backoffice/utils_proctorU_api.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import datetime
+import dateutil
 import json
 import logging
 
@@ -8,7 +9,7 @@ import requests
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-
+from django.utils.translation import ugettext as _
 
 logger = logging.getLogger(__name__)
 
@@ -59,9 +60,6 @@ def is_in_prod():
 
     :return: boolean are we in preprod / prod or not
     """
-    """
-    :return:
-    """
     try:
         settings.TEST_ROOT  # are we in test mode ?
     except AttributeError:
@@ -72,7 +70,7 @@ def is_in_prod():
     if test:
         return False  # if we are in test, we are not in prod
 
-    return (not settings.DEBUG)  # if we are in debug mode, we are not in prod
+    return not settings.DEBUG  # if we are in debug mode, we are not in prod
 
 
 def extract_infos(report):
@@ -82,7 +80,7 @@ def extract_infos(report):
         "UniqueId": report["UniqueId"],
         "ReservationNo": report["ReservationNo"],
         "CheckID": report["CheckID"],
-        "StartDate": report["StartDate"],
+        "StartDate": format_date(report["StartDate"]),
         "Authenticated": report["Authenticated"],
         "IncidentReport": report["IncidentReport"],
         "Escalated": report["Escalated"],
@@ -102,7 +100,7 @@ def get_protectU_students(course_name, course_run, student_grades):
         return student_activity
 
     filtered_reports = filter_reports_for_course(course_name, course_run, data, student_activity)
-    if "error" in filtered_reports:
+    if "error" in filtered_reports or "warn" in filtered_reports:
         return filtered_reports
 
     return aggregate_reports_per_user(filtered_reports, student_grades)
@@ -122,19 +120,21 @@ def aggregate_reports_per_user(filtered_reports, student_grades):
     """
     filtered_reports.sort(key=lambda d: d["Student"])
 
-    id_ = filtered_reports[0]["UniqueId"]
+    first_report = filtered_reports[0]
+    id_ = int(first_report["UniqueId"])
     identifiers = [id_]
     event_users = defaultdict(list)
-    event_users[id_].append(extract_infos(filtered_reports[0]))
+    event_users[id_].append(extract_infos(first_report))
 
     prec_json = filtered_reports[0]
     for report in filtered_reports[1:]:
         tmp = extract_infos(report)
         if report != prec_json:
-            event_users[report["UniqueId"]].append(tmp)
-            identifiers.append(report["UniqueId"])
+            event_users[int(report["UniqueId"])].append(tmp)
+            identifiers.append(int(report["UniqueId"]))
         prec_json = report
 
+    event_users = dict(event_users)
     if not is_in_prod():
         fun_users = User.objects.filter(last_name__in=identifiers)
     else:
@@ -142,7 +142,7 @@ def aggregate_reports_per_user(filtered_reports, student_grades):
 
     for user in fun_users:
         if not is_in_prod():
-            id_ = user.last_name
+            id_ = int(user.last_name)  # Ugly! : in dev we added the production primary key in the filed last name
         else:
             id_ = user.id
 
@@ -157,7 +157,7 @@ def aggregate_reports_per_user(filtered_reports, student_grades):
         else:
             event_users[id_][0]["fun_exam_grade"] = grade
             event_users[id_][0]["fun_exam_pass"] = passed
-    return dict(event_users)
+    return event_users
 
 
 def filter_reports_for_course(course_name, course_run, api_query, student_activity):
@@ -183,9 +183,14 @@ def filter_reports_for_course(course_name, course_run, api_query, student_activi
                                                                    api_query["start_date"],
                                                                    api_query["end_date"])
         logger.info(mess)
-        return {"error": mess}
+        return {"warn": {"id": exam_id,
+                         "start": format_date(api_query["start_date"]),
+                         "end": format_date(api_query["end_date"])}}
     return filtered_reports
 
+
+def format_date(date_str):
+    return dateutil.parser.parse(date_str).strftime(_('%m/%d/%y %H:%M'))
 
 def request_infos():
     start = datetime.datetime.today() - datetime.timedelta(days=100)
