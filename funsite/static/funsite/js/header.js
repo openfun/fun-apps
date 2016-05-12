5d7519beadee6c1e5498e91c7e3e6c8ea764ff7c
Régis Behmo <regis@behmo.com>
2016-05-12 09:59:11 +0200

    Verify T&C compliance on student dashboard page only
    We need to verify the compliance of T&Cs for all connected users.
    Previously, this was done for all users, on all pages and resulted in a
    great number of http requests (one for every user and every page) and
    sql requests (3 for every connected users, including on the
    CourseEnrollment table which does not have an index for the 'mode'
    column). To decrease the load, we check the compliance of payment terms
    T&C on the dashboard page only, which is displayed to logged-in users
    only.

--- funsite/static/funsite/js/header.js
+++ funsite/static/funsite/js/header.js
@@ -105,11 +105,12 @@ function checkCookie(name) {
 
     // if user need to accept newer version of payment terms and condition
     // redirect him to terms page.
-    if ((window.location.pathname !== '/payment/terms/agreement/') && (!checkCookie('verified_certificate'))) {
+    // Only check and T&C for logged-in users on the dashboard page
+    if ((window.location.pathname === '/dashboard') && (!checkCookie('verified_certificate'))) {
         $.get("/payment/terms/get/?no-text=",
             function(data) {
                 if (data.version) {
-                    window.location.pathname = '/payment/terms/agreement/'
+                    window.location.pathname = '/payment/terms/agreement/';
                 }
             }
         );
