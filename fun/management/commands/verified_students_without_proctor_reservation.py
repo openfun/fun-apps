23154037a59a9c39dd87bdd9ab8f4369303a679b
Matthieu Falce <falce.matthieu@gmail.com>
2016-06-07 11:37:26 +0200

    cetification: list verified users without proctoru reservation
    The list is obtained with a management command.
    
    This is a work in progress, we should have a more fine grained selection
    to catch all people.
    
    This closes #2943

---
+++ fun/management/commands/verified_students_without_proctor_reservation.py
@@ -0,0 +1,56 @@
+# pylint: disable=missing-docstring
+
+import csv
+from optparse import make_option
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
+
+from backoffice.certificate_manager.verified import get_enrolled_verified_students
+from backoffice.utils import get_course
+
+from proctoru.models import ProctoruUser
+
+
+def write_csv(file_handler, header, rows):
+    writer = csv.writer(file_handler)
+    writer.writerow(header)
+    for row in rows:
+        writer.writerow(row)
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--filename',
+                    default=False,
+            help='File where the output will be written / defaults to stdout when not provided'),
+        make_option("--course-key-string",
+            help='CourseKeyString of the course to analyse'),
+    )
+
+    def handle(self, *args, **options):
+        print(options)
+        if options["course_key_string"]:
+            course_key_string = options["course_key_string"]
+        else:
+            raise CommandError("Option `--course-key-string=...` must be specified.")
+
+        course = get_course(course_key_string)
+
+        verified_students = get_enrolled_verified_students(course.id).select_related("profile")
+        proctoru_students = ProctoruUser.objects.filter(student__in=verified_students).values_list("student", flat=True)
+
+        # TODO : not the best way to get students without reservations :
+        #  * inscriptions in proctoru model is not bound to course (false negative)
+        #  * we can't spot people who registered and cancelled their reservation
+        # Thus we can fail to spot some people without reservations
+        enrolled_students_not_proctoru = set(verified_students) - set(proctoru_students)
+
+        header = ("Name", "Username", "Email")
+        rows = [(s.profile.name, s.username, s.email) for s in enrolled_students_not_proctoru]
+
+        if options['filename']:
+            with open(options['filename'], "w") as f:
+                write_csv(f, header, rows)
+        else:
+            write_csv(sys.stdout, header, rows)
