0280bb0403d6cd211601a05ea4a9d529df663ea6
Richard Moch <Richard.moch@gmail.com>
2016-07-04 14:12:20 +0000

    direct_to_template do not exists any more

--- contact/urls.py
+++ contact/urls.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from contact import views
 
 # namespace='contact'
-
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns('',
     url(r'^contact/?$', views.ContactFormView.as_view(), name="contact"),
-    url(r'^contact/sent/$', 'django.views.generic.simple.direct_to_template',
-        {'template': 'contact/contact_form_sent.html'}, name='contact_form_sent'),
+    url(r'^contact/sent/$', TemplateView.as_view(template_name='contact/contact_form_sent.html'), name='contact_form_sent'),
 )
