5375a06e8da8d6293321e73eea3eb1b72d148c41
Richard Moch <Richard.moch@gmail.com>
2016-07-04 14:12:20 +0000

    direct_to_template do not exists any more

--- funsite/urls.py
+++ funsite/urls.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 
 static_pages = ['about', 'honor', 'legal', 'privacy', 'tos', 'register_info',
         'proctoru']
 
 urls = [
-    url(r'^{}/?$'.format(name), 'direct_to_template',
-        kwargs={'template': 'funsite/static_templates/%s.html' % name},
-        name=name)
+    url(r'^{}/?$'.format(name), TemplateView.as_view(template_name='funsite/static_templates/%s.html' % name), name=name)
     for name in static_pages
 ]
 
 urls.append(
-    url(r'^searchprovider.xml$', 'direct_to_template',
-        kwargs={
-            'template': 'funsite/static_templates/searchprovider.xml',
-            'mimetype': 'text/xml',
-        },
+    url(r'^searchprovider.xml$', TemplateView.as_view(template_name='funsite/static_templates/searchprovider.xml', content_type='text/xml'),
         name='searchprovider.xml')
 )
 
