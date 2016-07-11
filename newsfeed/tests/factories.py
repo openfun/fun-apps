b0c98906d0e6eb1a6c5e75902c740c5d60e4f1bc
Richard Moch <Richard.moch@gmail.com>
2016-07-11 09:34:22 +0000

    update factories regarding factory-boy upgrade

--- newsfeed/tests/factories.py
+++ newsfeed/tests/factories.py
@@ -7,16 +7,19 @@ from newsfeed import models
 
 
 class ArticleCategoryFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = models.ArticleCategory
     slug = factory.Sequence(lambda n: 'category-{0}'.format(n))
+
+    class Meta(object):
+        model = models.ArticleCategory
 
 
 class ArticleFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = models.Article
-
     # the number can't be at the end, otherwise, "news1" is in "news10"
     title = factory.Sequence(lambda n: u"-- An awesome piece of news n° {} --".format(n))
     slug = factory.Sequence(lambda n: 'a-great-slug-{0}'.format(n))
     text = u"J'étais un texte accentué !"
     created_at = factory.lazy_attribute(lambda x: datetime.datetime.now())
     category = None
+
+    class Meta(object):
+        model = models.Article
