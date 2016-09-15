4124e85dc5822fbd44b8720a1992e265e94c5103
Matthieu Falce <falce.matthieu@gmail.com>
2016-09-15 17:03:26 +0200

    update data migration script to not crash when no data.
    Closes #3188

--- courses/migrations/0002_is_visible_deprecated.py
+++ courses/migrations/0002_is_visible_deprecated.py
@@ -16,14 +16,25 @@ def update_visibility(mongo_course, visibility, user_id):
         modulestore().update_item(mongo_course, user_id)
 
 
+def get_first_superuser():
+    """Returns first superuser or None."""
+    first_superuser = User.objects.filter(is_superuser=True).first()
+    return first_superuser
+
+
 def reversed_update(apps, schema_editor):
     """ reverse the migration : we put the mongo value to the default value (ie : CATALOG_VISIBILITY_CATALOG_AND_ABOUT)"""
 
     mongo_courses = modulestore().get_courses()
-    user_id = User.objects.filter(is_superuser=True).first().id
-    for mongo_course in mongo_courses:
-        visibility = CATALOG_VISIBILITY_CATALOG_AND_ABOUT
-        update_visibility(mongo_course, visibility, user_id)
+
+    # we need a superuser to manipulate mongo. Nevertheless, if there is none, there are no data in the base, so we don't need to make the migration
+    user = get_first_superuser()
+    if user:
+        user_id = user.id
+
+        for mongo_course in mongo_courses:
+            visibility = CATALOG_VISIBILITY_CATALOG_AND_ABOUT
+            update_visibility(mongo_course, visibility, user_id)
 
 
 def update_mongo_courses(apps, schema_editor):
@@ -32,24 +43,29 @@ def update_mongo_courses(apps, schema_editor):
     """
 
     mongo_courses = modulestore().get_courses()
-    user_id = User.objects.filter(is_superuser=True).first().id
-    fun_courses = Course.objects.all()
 
-    for mongo_course in mongo_courses:
-        course_id = mongo_course.id
-        print("updating {}".format(course_id))
+    # we need a superuser to manipulate mongo. Nevertheless, if there is none, there are no data in the base, so we don't need to make the migration
+    user = get_first_superuser()
+    if user:
+        user_id = user.id
 
-        try:
-            fun_course = fun_courses.get(key=course_id)
-        except Course.DoesNotExist:
-            pass
-            # if course is not found in fun, we let the default values in mongo
-            print("course not found {}".format(course_id))
-        else:
-            visibility = CATALOG_VISIBILITY_NONE
-            if fun_course.show_in_catalog:
-                visibility = CATALOG_VISIBILITY_CATALOG_AND_ABOUT
-            update_visibility(mongo_course, visibility, user_id)
+        fun_courses = Course.objects.all()
+
+        for mongo_course in mongo_courses:
+            course_id = mongo_course.id
+            print("updating {}".format(course_id))
+
+            try:
+                fun_course = fun_courses.get(key=course_id)
+            except Course.DoesNotExist:
+                pass
+                # if course is not found in fun, we let the default values in mongo
+                print("course not found {}".format(course_id))
+            else:
+                visibility = CATALOG_VISIBILITY_NONE
+                if fun_course.show_in_catalog:
+                    visibility = CATALOG_VISIBILITY_CATALOG_AND_ABOUT
+                update_visibility(mongo_course, visibility, user_id)
 
 
 class Migration(migrations.Migration):
