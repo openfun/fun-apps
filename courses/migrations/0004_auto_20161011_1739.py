openfun/fun-apps

commit def7d8fa39c78dd344625c9664d3896e9b47e530

Django warning: null has no effect on ManyToManyField

courses/models.py
@@ -37,8 +37,7 @@ class Course(models.Model):
     image_url = models.CharField(_(u'image url'), max_length=255, blank=True)
     universities = models.ManyToManyField('universities.University',
         through='CourseUniversityRelation', related_name='courses')
-    subjects = models.ManyToManyField('CourseSubject', related_name='courses',
-        null=True, blank=True)
+    subjects = models.ManyToManyField('CourseSubject', related_name='courses')
     level = models.CharField(_('level'), max_length=255,
         choices=courses_choices.COURSE_LEVEL_CHOICES, blank=True, db_index=True)
     language = models.CharField(_('language'), max_length=255,

newsfeed/models.py
@@ -84,8 +84,7 @@ class Article(models.Model):
     category = models.ForeignKey('ArticleCategory', verbose_name=_('category'),
         null=True, blank=True, related_name='articles')
     courses = models.ManyToManyField('courses.Course', verbose_name=_('courses'),
-        related_name='articles', null=True, blank=True,
-        limit_choices_to={'is_active': True})
+        related_name='articles', limit_choices_to={'is_active': True})
     thumbnail = models.ImageField(_('thumbnail'),
         upload_to='newsfeed', null=True, blank=True,
         help_text=_('Displayed on the news list page.'))

commit ad0c603c35c8b7d0cc9fcb52f5917731d7518ebf

FIX regression introduced by def7d8f while upgrading to Django 1.8. Those 2 ManyToMany relations are NOT required Closes #3250

courses/migrations/0004_auto_20161011_1739.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0003_default_value_to_featured'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='subjects',
+            field=models.ManyToManyField(related_name='courses', to='courses.CourseSubject', blank=True),
+        ),
+    ]

courses/models.py
@@ -37,7 +37,7 @@ class Course(models.Model):
     image_url = models.CharField(_(u'image url'), max_length=255, blank=True)
     universities = models.ManyToManyField('universities.University',
         through='CourseUniversityRelation', related_name='courses')
-    subjects = models.ManyToManyField('CourseSubject', related_name='courses')
+    subjects = models.ManyToManyField('CourseSubject', related_name='courses', blank=True)
     level = models.CharField(_('level'), max_length=255,
         choices=courses_choices.COURSE_LEVEL_CHOICES, blank=True, db_index=True)
     language = models.CharField(_('language'), max_length=255,

newsfeed/migrations/0002_auto_20161011_1724.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('newsfeed', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='article',
+            name='courses',
+            field=models.ManyToManyField(related_name='articles', verbose_name='courses', to='courses.Course', blank=True),
+        ),
+    ]

newsfeed/models.py
@@ -84,7 +84,7 @@ class Article(models.Model):
     category = models.ForeignKey('ArticleCategory', verbose_name=_('category'),
         null=True, blank=True, related_name='articles')
     courses = models.ManyToManyField('courses.Course', verbose_name=_('courses'),
-        related_name='articles', limit_choices_to={'is_active': True})
+        related_name='articles', limit_choices_to={'is_active': True}, blank=True)
     thumbnail = models.ImageField(_('thumbnail'),
         upload_to='newsfeed', null=True, blank=True,
         help_text=_('Displayed on the news list page.'))