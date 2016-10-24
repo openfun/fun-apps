b6a3666c19410666f04727da700bb35979677746
Richard Moch <richard.moch@gmail.com>
2016-10-24 14:01:12 +0000

    Add minimum human interface to VideofrontCourseSettings model

--- videoproviders/admin.py
+++ videoproviders/admin.py
@@ -12,9 +12,14 @@ class YoutubeAuthAdminInline(admin.StackedInline):
     fields = ("client_id", "client_secret", "access_token", "refresh_token", "token_expiry",)
 
 
-class YoutubeCourseSettingsAdmin(admin.ModelAdmin):
-    list_display = ('course_id', 'playlist_id')
 
+class VideofrontCourseSettingsAdmin(admin.ModelAdmin):
+    list_display = ('course_id', 'playlist_id')
+    search_fields = ('course_id', 'playlist_id')
+
+
+class YoutubeCourseSettingsAdmin(VideofrontCourseSettingsAdmin):
+    pass
 
 class VideofrontAuthAdminInline(admin.StackedInline):
     model = models.VideofrontAuth
@@ -23,3 +28,5 @@ class VideofrontAuthAdminInline(admin.StackedInline):
 
 admin.site.register(models.VideoUploaderDeactivationPeriod, VideoUploaderDeactivationPeriodAdmin)
 admin.site.register(models.YoutubeCourseSettings, YoutubeCourseSettingsAdmin)
+
+admin.site.register(models.VideofrontCourseSettings, VideofrontCourseSettingsAdmin)
