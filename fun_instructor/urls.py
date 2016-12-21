b08dd8b2779da112a9a16f4eb2de3c417d4abdf1
Richard Moch <Richard.moch@gmail.com>
2016-12-21 16:54:00 +0000

    Adapt fun_instructor application to Eucalyptus

--- fun_instructor/urls.py
+++ fun_instructor/urls.py
@@ -2,6 +2,9 @@
 
 from django.conf.urls import url, patterns
 
+
 urlpatterns = patterns( '',
+# This url overrides edx'X ajax view which populate list of grades report available to download on teacher's dashboard.
+# See views.py
 url(r'^list_report_downloads$',
         'fun_instructor.views.list_report_downloads', name="list_report_downloads"),)
