7b537662589671565a379c5d424c91e31ed2cfcc
Laurent David <lmedavid@gmail.com>
2017-07-26 14:57:17 +0200

    Bokecc uploader: Refactor bokecc client so we can share common code between client and command line

--- videoproviders/api/bokecc.py
+++ videoproviders/api/bokecc.py
@@ -13,18 +13,10 @@ logger = logging.getLogger(__name__)
 
 class Client(BaseClient):
     """
-    This is the client for the Bokecc video provider. 
+    This is the client for the Bokecc video provider.
     There is only one account per platform's instance. Video are stored under
     different categories representing each university
     """
-
-    BOKECC_PLAYER_ID = getattr(settings, 'BOKECC_PLAYER_ID', '30130B6BCFC187A4')
-    BOKECC_URL = getattr(settings, 'BOKECC_URL', 'http://spark.bokecc.com/api/')
-
-    # This is a complete hack: we need to have a video id to create a playlist
-    # So this is an id of a small video that will always be part of any playlist
-    # Warning: if this video is deleted we will not be able to create a playlist
-    BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK = getattr(settings, 'BOKECC_FAKE_VIDEO_ID', '448AA25E88D95C639C33DC5901307461')
 
     def __init__(self, course_key_string):
         super(Client, self).__init__(course_key_string)
@@ -38,7 +30,7 @@ class Client(BaseClient):
         if self._playlist_id is None:
             # Build the playlist if from the course_key_string and check if any
             # Bokecc playlist exists with this name
-            self._playlist_id = self.__get_or_create_playlist()
+            self._playlist_id = BokeccUtil.get_or_create_playlist(self.course_id)
         return self._playlist_id
 
     ####################
@@ -48,8 +40,7 @@ class Client(BaseClient):
     def get_auth(self):
         # Here there is no specific Auth credentials except the ones provided in the general
         # Settings (see BOKECC_APIKEY and BOKECC_USERID)
-        api_salt_key = getattr(settings, 'BOKECC_APIKEY', '')
-        user_id = getattr(settings, 'BOKECC_USERID', '')
+        api_salt_key, user_id = BokeccUtil.get_auth()
         return {"user_id": user_id, "salt_key": api_salt_key}
 
     def get_video(self, video_id, player_width="890px", player_height="375px"):
@@ -61,15 +52,13 @@ class Client(BaseClient):
         #         "playcode":"<script src='xxx' ...</script>"
         #     }
         # }
-        bokecc_video_playcode = self.__bokecc_request_get(
-            self.BOKECC_URL,
+        bokecc_video_playcode = BokeccUtil.bokecc_request_get(
             'video/playcode',
             params={'videoid': video_id,
-                    'playerid': self.BOKECC_PLAYER_ID,
+                    'playerid': BokeccUtil.BOKECC_PLAYER_ID,
                     'auto_play': 'false',
                     'player_width': player_width,
-                    'player_height': player_height,
-                    }
+                    'player_height': player_height}
         )
         # Get video info - Format :
         # {
@@ -90,8 +79,7 @@ class Client(BaseClient):
         #         ]
         #     }
         # }
-        bokecc_video_infos = self.__bokecc_request_get(
-            self.BOKECC_URL,
+        bokecc_video_infos = BokeccUtil.bokecc_request_get(
             'video',
             params={'videoid': video_id, }
         )
@@ -117,14 +105,13 @@ class Client(BaseClient):
     def iter_videos(self):
         """
         Iterates through the videos
-        
         :return: a list of videos  
         """
-        playlist_id = self.__get_or_create_playlist()
+
+        playlist_id = BokeccUtil.get_or_create_playlist(self.course_id)
         # Return all videos in playlist
         if playlist_id is not None:
-            response = self.__bokecc_request_get(
-                self.BOKECC_URL,
+            response = BokeccUtil.bokecc_request_get(
                 'playlist/update',
                 params={'playlistid': playlist_id, }
             )
@@ -133,7 +120,7 @@ class Client(BaseClient):
                 for video in video_id_list:
                     # Get full info for this video
                     video_id = video["id"]
-                    if video_id != self.BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK:
+                    if video_id != BokeccUtil.BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK:
                         yield self.get_video(video_id)
 
     def delete_video(self, video_id):
@@ -142,8 +129,7 @@ class Client(BaseClient):
                Returns:
                    None
         """
-        response = self.__bokecc_request_get(
-            self.BOKECC_URL,
+        response = BokeccUtil.bokecc_request_get(
             'video/delete',
             params={'videoid': video_id, }
         )
@@ -152,8 +138,7 @@ class Client(BaseClient):
 
     def update_video_title(self, video_id, title):
         """Change a video title"""
-        response = self.__bokecc_request_get(
-            self.BOKECC_URL,
+        response = BokeccUtil.bokecc_request_get(
             'video/update',
             params={
                 'videoid': video_id,
@@ -199,8 +184,7 @@ class Client(BaseClient):
         pass
 
     def create_video(self, payload, title=None):
-        response = self.__bokecc_request_get(
-            self.BOKECC_URL,
+        response = BokeccUtil.bokecc_request_get(
             'video/create',
             params={'title': title.encode('utf-8'),
                     'tag': '',
@@ -212,23 +196,71 @@ class Client(BaseClient):
             raise ClientError("Error creating videos")
         return response
 
+
+class BokeccUtil:
+    BOKECC_PLAYER_ID = getattr(settings, 'BOKECC_PLAYER_ID', '30130B6BCFC187A4')
+    BOKECC_URL = getattr(settings, 'BOKECC_URL', 'http://spark.bokecc.com/api/')
+
+    # This is a complete hack: we need to have a video id to create a playlist
+    # So this is an id of a small video that will always be part of any playlist
+    # Warning: if this video is deleted we will not be able to create a playlist
+    BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK = getattr(settings, 'BOKECC_FAKE_VIDEO_ID', '448AA25E88D95C639C33DC5901307461')
+
+    @staticmethod
+    def get_or_create_playlist( course_id):
+        playlist_id = None
+        try:
+            # Read from BokeCCSettings object
+            course_settings = BokeCCCourseSettings.objects.get(course_id=course_id)
+            playlist_id = course_settings.playlist_id
+        except BokeCCCourseSettings.DoesNotExist:
+            # Then look for it on the Bokecc end point
+            response = BokeccUtil.bokecc_request_get(
+                'playlists',
+                params={}
+            )
+
+            if 'playlists' in response:
+                for playlist in response['playlists']['playlist']:
+                    if playlist["name"] == str(course_id):
+                        playlist_id = playlist["id"]
+                        break
+
+            # We have not found the playlist on the remote server, so we need to create it
+            if playlist_id is None:
+                # create the given playlist
+                response = BokeccUtil.bokecc_request_get(
+                    'playlist/create',
+                    params={'name': str(course_id),
+                            'videoid': BokeccUtil.BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK,
+                            }
+                )
+                if "playlist" in response:
+                    playlist_id = response["playlist"]["id"]
+            if playlist_id is not None:
+                course_settings, _created = BokeCCCourseSettings.objects.get_or_create(course_id=course_id)
+                course_settings.playlist_id = playlist_id
+                course_settings.save()
+            else:
+                raise ClientError("Impossible to create playlist for course")
+        return playlist_id
+
     ####################
     # Utilities
     ####################
-
-    def __bokecc_request_get(self, bokecc_base_url, endpoint, params=None):
+    @staticmethod
+    def bokecc_request_get(endpoint, params=None):
 
         if params is None:
             params = []
-        api_key_salt = self.auth["salt_key"]
-        bokecc_user_id = self.auth["user_id"]
 
+        api_key_salt,bokecc_user_id = BokeccUtil.get_auth()
         full_req_params = params
         full_req_params['userid'] = bokecc_user_id
         full_req_params['format'] = 'json'
 
-        url = bokecc_base_url + endpoint + '?'
-        complete_url = url + self.__bokecc_get_hqs(full_req_params, api_key_salt)
+        url = BokeccUtil.BOKECC_URL + endpoint + '?'
+        complete_url = url + BokeccUtil.__bokecc_get_hqs(full_req_params, api_key_salt)
         f = urllib.urlopen(complete_url)
         response = json.loads(f.read())
 
@@ -257,42 +289,9 @@ class Client(BaseClient):
         hqs = '&'.join((qs, qftime, hashqf))
         return hqs
 
-    def __get_or_create_playlist(self):
-        playlist_id = None
-        try:
-            # Read from BokeCCSettings object
-            course_settings = BokeCCCourseSettings.objects.get(course_id=self.course_id)
-            playlist_id = course_settings.playlist_id
-        except BokeCCCourseSettings.DoesNotExist:
-            # Then look for it on the Bokecc end point
-            response = self.__bokecc_request_get(
-                self.BOKECC_URL,
-                'playlists',
-                params={}
-            )
+    @staticmethod
+    def get_auth():
+        api_salt_key = getattr(settings, 'BOKECC_APIKEY', '')
+        user_id = getattr(settings, 'BOKECC_USERID', '')
+        return api_salt_key, user_id
 
-            if 'playlists' in response:
-                for playlist in response['playlists']['playlist']:
-                    if playlist["name"] == self.course_key_string:
-                        playlist_id = playlist["id"]
-                        break
-
-            # We have not found the playlist on the remote server, so we need to create it
-            if playlist_id is None:
-                # create the given playlist
-                response = self.__bokecc_request_get(
-                    self.BOKECC_URL,
-                    'playlist/create',
-                    params={'name': self.course_key_string,
-                            'videoid': self.BOKECC_PLAYLIST_FAKE_VIDEO_ID_HACK,
-                            }
-                )
-                if "playlist" in response:
-                    playlist_id = response["playlist"]["id"]
-            if playlist_id is not None:
-                course_settings, _created = BokeCCCourseSettings.objects.get_or_create(course_id=self.course_id)
-                course_settings.playlist_id = playlist_id
-                course_settings.save()
-            else:
-                raise ClientError("Impossible to create playlist for course")
-        return playlist_id
--- videoproviders/management/commands/videofront-to-bokecc.py
+++ videoproviders/management/commands/videofront-to-bokecc.py
@@ -6,6 +6,17 @@ import xmodule.modulestore.django
 from videoproviders.api import ClientError
 
 from videoproviders.api.videofront import Client as videofrontclient
+from videoproviders.api.bokecc import BokeccUtil
+
+import requests
+import json
+import os
+from tempfile import NamedTemporaryFile
+import hashlib
+from time import time
+import urllib
+
+from django.conf import settings
 
 class Command(BaseCommand):
     help = """
@@ -18,6 +29,11 @@ class Command(BaseCommand):
                     dest='courseid',
                     default=None,
                     help='Course to fetch videos from'),
+        make_option('-s', '--chunksize',
+                    metavar='CHUNKSIZE',
+                    dest='chunksize',
+                    default=(512 * 1024),
+                    help='Chunk size for upload and download'),
     )
 
     def handle(self, *args, **options):
@@ -25,7 +41,7 @@ class Command(BaseCommand):
         course_key_string = options['courseid']
         if not course_key_string:
             raise CommandError("You must specify a course id")
-
+        chunksize = options['chunksize']
 
         course_id = CourseKey.from_string(course_key_string)
         store = xmodule.modulestore.django.modulestore()
@@ -45,10 +61,42 @@ class Command(BaseCommand):
                     video = videofront_client.get_video_with_subtitles(xblock.video_id)
                     hdsource = filter(lambda vs: vs['res'] == 5400 , video['video_sources'])
                     if len(hdsource)>=1 :
-                        print 'hdsource' , hdsource[0]['url']
+                        print 'Video hdsource' , hdsource[0]['url']
+                        self.process_video(video['video_sources'][0]['url'], video['title'], video['id'], course_id,chunksize)
+                        #self.process_video(hdsource[0]['url'],video['title'],video['id'],chunksize)
             except ClientError as e:
                 print 'Error fetching video ({0}) Message ({1})'.format(video_id, e.message)
 
+    def process_video(self,videofronthdurl,videofronttitle,videofrontid,course_id, cs):
+        local_filename = videofronthdurl.split('/')[-1]
+
+        print 'Processing video at {0} with name:{1} and id:{2} '.format(videofronthdurl, videofronttitle, videofrontid)
+
+        bcc = BokeccVideoUploader(cs)
+        # Check first if video exists already on bokeecc
+        videoexists = bcc.check_video_exists(videofrontid)
+        if videoexists is not None:
+            print 'Video with videofront Id:{1} is already on Bokecc with ID {2} '.format(videofrontid,videoexists['id'])
+
+        # If not then download it from videofront
+        r = requests.get(videofronthdurl, stream=True)
+        f = NamedTemporaryFile()
+        chunkcount = 0
+        filesize = r.headers['Content-length']
+        for chunk in r.iter_content(chunk_size=cs):
+            if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    chunkcount += 1
+                    print '(' + str(chunkcount) +'/' + str(int(filesize)/cs) + ')',
+
+        # Use title as videofrontid as it is the only field that is searcheable later
+        # Keep the id in description just in case some would rename the files
+        video = bcc.create_video(videofrontid,f.tell(),local_filename,videofrontid)
+        if video:
+            bcc.upload_video(video['videoid'],video['chunkurl'],f)
+            # Make sure we change this video an put in in the course playlist
+            bcc.set_video_in_playlist(video['videoid'],course_id)
+        f.close()
 
 def xblock_ancestry(item):
     from xmodule.modulestore.exceptions import ItemNotFoundError
@@ -60,3 +108,83 @@ def xblock_ancestry(item):
     if parent_ancestry:
         parent_ancestry += u"/"
     return u"{}{}".format(parent_ancestry, item.display_name)
+
+class BokeccVideoUploader:
+
+    def __init__(self, chunksize):
+        self.api_salt_key, self.user_id = BokeccUtil.get_auth()
+        self.chunksize = chunksize
+
+    def create_video(self, title, filesize, filename, desc):
+        bokecc_video = BokeccUtil.bokecc_request_get(
+            'video/create',
+            params={
+                'userid' : self.user_id,
+                'title': title,
+                'filesize' : filesize,
+                'filename' :  filename,
+                'format': 'json',
+                'description': desc
+            }
+        )
+        if not 'error' in bokecc_video:
+                if "uploadinfo" in bokecc_video:
+                    return bokecc_video['uploadinfo']
+        return None
+
+    def upload_video(self, videoid, uploadchunkurl, videofile):
+        fsize = videofile.tell()
+        videofile.seek(0)
+        chunk = videofile.read(self.chunksize)
+        chunkstart = 0
+        while chunk:
+            data = {
+                   'ccvid': videoid,
+                   'format': 'json',
+            }
+            file = { 'file': (videofile.name, chunk, 'application/octet-stream')}
+            contentrange = "bytes "+ str(chunkstart) + "-" + str(videofile.tell()) + "/" + str(fsize)
+            header = {"Content-Range": contentrange}
+            p = requests.post(uploadchunkurl, files=file, data=data,headers=header)
+            print 'Uplodading file {0} to {1} - at {2} '.format(videofile.name,uploadchunkurl,chunkstart)
+            chunkstart = videofile.tell()
+            chunk = videofile.read(self.chunksize)
+
+    def set_video_in_playlist(self,videoid,course_id):
+        playlistid = BokeccUtil.get_or_create_playlist(course_id)
+        response = BokeccUtil.bokecc_request_get(
+            'playlist/update',
+            params={'playlistid': playlistid,
+                    'userid' : self.user_id,
+                    'format': 'json'
+                    }
+        )
+        if not 'error' in response:
+            videos = response['video']
+            videoliststring = ''
+            for video in videos:
+                if not videoliststring == '':
+                    videoliststring += ','
+                videoliststring += video['id']
+            response = BokeccUtil.bokecc_request_get(
+                'playlist/update',
+                params={'playlistid': playlistid,
+                        'userid': self.user_id,
+                        'video': videoliststring,
+                        'format': json
+                       }
+                )
+            return response
+
+    def check_video_exists(self, videofrontid):
+        response = BokeccUtil.bokecc_request_get(
+            'videos/search',
+            params={'userid': self.user_id,
+                    'q': 'TITLE:'+videofrontid,
+                    'format': 'json'
+                    }
+        )
+        if not 'error' in response:
+            if  response['videos']['total'] > 0:
+                response['videos']['video'][0]
+        return None
