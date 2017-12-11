33449ed703b5d593af95330fdd70a5a57467ddec
Richard Moch <richard.moch@gmail.com>
2017-12-11 15:18:02 +0000

    Simplify code causing bad redirection in developpement environment

--- fun/middleware/enforce_legal_acceptance.py
+++ fun/middleware/enforce_legal_acceptance.py
@@ -3,72 +3,61 @@
 conditions are published"""
 
 import re
-from django.conf import settings
+
 from django.shortcuts import redirect
+
 from payment.models import legal_acceptance
 
-DEFAULT_AGREEMENT_WHITELIST = map(
+AGREEMENT_WHITELIST = map(
     re.compile,
     (
-        r"""^/payment/.*""",
-        r"""^/xblock/.*""",
-        r"""^/account/setting.*""",
-        r""".*accept.*""",
-        r"""^/(news|login|contact|about|help|legal|privacy|honor|charte)/?$""",
-        r""".*/login_ajax.*""",
-        r"""^/static.*""",
-        r""".*search.*""",
-        r""".*revision.*""",
-        r"""^/static/.*""",
-        r"""^/api/user/.*""",
-        r"""^/c4x.*""",
-        r""".*logout.*$""",
-        r""".*asset.*""",
-        r""".*\.(png|font|html|js|css|jpeg|pdf|gif|mp4).*""",
-        r"""^/?$""",
+        r'^/payment/.*',
+        r'^/xblock/.*',
+        r'^/account/setting.*',
+        r'.*accept.*',
+        r'^/(news|login|contact|about|help|legal|privacy|honor|charte)/?$',
+        r'.*/login_ajax.*',
+        r'^/static.*',
+        r'.*search.*',
+        r'.*revision.*',
+        r'^/static/.*',
+        r'^/api/user/.*',
+        r'^/c4x.*',
+        r'.*logout.*$',
+        r'.*asset.*',
+        r'.*\.(png|font|html|js|css|jpeg|pdf|gif|mp4).*',
+        r'^/?$',
     )
 )
-def get(self, key, default):
-    return getattr(self, key) if hasattr(self, key) else default
 
-LMS_BASE = get(settings, "LMS_BASE", "")
-PROT = get(settings, "HTTPS", "off") == "on" and "s" or ""
-LMS_REDIR = LMS_BASE and ("http%s://%s/" % (PROT, LMS_BASE)) or ""
-DEFAULT_AGREEMENT_FORM = "%s/payment/terms/" % (LMS_REDIR)
+TERMS_AND_CONDITIONS_AGREEMENT = '/payment/terms/'
 
-def terms_accepted(func):
+
+def ensure_terms_accepted(func):
     """
     force redirect to acceptance of legal condition if
     latest not accepted
-    redirection landing page SHOULD be set in settings
-    AGREEMENT_FORM else defaults to DEFAULT_AGREEMENT_FORM
     """
     def wrapped(request, *args, **kwargs):
-
         if not legal_acceptance(request.user):
-            return redirect(
-                get(settings, "AGREEMENT_FORM", DEFAULT_AGREEMENT_FORM),
-                *args, **kwargs
-            )
+            return redirect(TERMS_AND_CONDITIONS_AGREEMENT, *args, **kwargs)
         else:
             return func(request, *args, **kwargs)
     return wrapped
 
+
 class LegalAcceptance(object):
-    def __init__(self, *a, **kw):
-        self.white_list = get(
-            settings,
-            "AGREEMENT_WHITELIST",
-            DEFAULT_AGREEMENT_WHITELIST
-        )
 
     def is_whitelisted(self, url):
-        return any(wl_url.match(url) for wl_url in self.white_list)
+        return any(wl_url.match(url) for wl_url in AGREEMENT_WHITELIST)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if not self.is_whitelisted(request.path_info) \
-            and hasattr(request, "user") \
-            and request.user.is_authenticated and \
-            not legal_acceptance(request.user):
-            return terms_accepted(view_func)(request, *view_args, **view_kwargs)
-
+        # We don't redirect the current user to the legal acceptance page if:
+        #   1. the current path is whitelisted,
+        #   2. the latest T&C were already accepted by the current user.
+        if (
+                not self.is_whitelisted(request.path_info) and
+                hasattr(request, "user") and
+                request.user.is_authenticated() and
+                not legal_acceptance(request.user)):
+            return ensure_terms_accepted(view_func)(request, *view_args, **view_kwargs)
