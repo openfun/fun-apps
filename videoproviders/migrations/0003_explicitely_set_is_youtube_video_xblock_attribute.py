18845262b8aae8fa17c9328da83d055a17e13e0a
Richard Moch <richard.moch@gmail.com>
2018-03-21 16:25:04 +0100

    Remove datamigrations which are useless at this state of the project and fail to run on empty database

--- videoproviders/migrations/0003_explicitely_set_is_youtube_video_xblock_attribute.py
+++ videoproviders/migrations/0003_explicitely_set_is_youtube_video_xblock_attribute.py
@@ -4,32 +4,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-def set_is_youtube_video_fields(apps, schema_editor):
-    """
-    Update video xblocks for all courses. Since the is_youtube_video field is
-    set to False by default, it is often not stored in Mongodb. This prevents
-    us from changing the default value for this field. To avoid this problem,
-    we explicitely set the value of this field in mongodb.
-    """
-    import xmodule.modulestore.django
-    modulestore = xmodule.modulestore.django.modulestore()
-    for course in modulestore.get_courses():
-        course_store = modulestore._get_modulestore_for_courselike(course.id)
-        if not hasattr(course_store, "collection"):
-            # Course is not stored in mongodb
-            continue
-        for category in ['video', 'libcast_xblock']:
-            course_store.collection.update(
-                {"_id.category": category, "metadata.is_youtube_video": {"$exists": False}},
-                {"$set": {"metadata.is_youtube_video": False}}
-            )
-
-def unset_is_youtube_video_fields(apps, schema_editor):
-    """
-    There is no need for a reverse mligration.
-    """
-    pass
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -37,5 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(set_is_youtube_video_fields, reverse_code=unset_is_youtube_video_fields),
     ]
