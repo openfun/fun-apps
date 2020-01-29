92a09f35dca99070db8b08d6afb157d8b042879b
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2020-01-29 17:35:37 +0100

    ⚡️(funsite) accelerate home page by overriding edX's view
    In the previous release, we cached the body of the home page template
    for authenticated users in an attempt to speed it up and found out that
    most of the time is spent in edX's view where they retrieve objects that
    we don't use in our home page. Adding our own view should further
    accelerate our home page.

--- funsite/urls.py
+++ funsite/urls.py
@@ -3,31 +3,42 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView, RedirectView
 
+from .views import index
 
-static_pages = ('about', 'register_info', )
-legal_pages =('honor','legal','privacy','tos', 'charte', )
+static_pages = ("about", "register_info")
+legal_pages = ("honor", "legal", "privacy", "tos", "charte")
 
-urls = tuple(
-    url(r'^{}/?$'.format(name), TemplateView.as_view(template_name='funsite/static_templates/%s.html' % name), name=name)
+urls = (url(r"^$", index, name="root"),)
+
+urls += tuple(
+    url(
+        r"^{}/?$".format(name),
+        TemplateView.as_view(template_name="funsite/static_templates/%s.html" % name),
+        name=name,
+    )
     for name in static_pages
 )
 
-urls += tuple([
-    url(
-        r'^{}/?$'.format(name),
-        RedirectView.as_view(
-            url='/payment/terms/#%s' % name, permanent=True), name=name
-    ) for name in legal_pages
-])
+urls += tuple(
+    [
+        url(
+            r"^{}/?$".format(name),
+            RedirectView.as_view(url="/payment/terms/#%s" % name, permanent=True),
+            name=name,
+        )
+        for name in legal_pages
+    ]
+)
 
 urls += (
-    url(r'^searchprovider.xml$',
+    url(
+        r"^searchprovider.xml$",
         TemplateView.as_view(
-            template_name='funsite/static_templates/searchprovider.xml',
-            content_type='text/xml'
+            template_name="funsite/static_templates/searchprovider.xml",
+            content_type="text/xml",
         ),
-        name='searchprovider.xml'
+        name="searchprovider.xml",
     ),
 )
 
-urlpatterns = patterns('django.views.generic.simple', *urls)
+urlpatterns = patterns("django.views.generic.simple", *urls)
