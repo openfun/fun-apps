c60d33445b1e2b97949cf0eda6a8becaf45ee325
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2020-01-29 17:35:37 +0100

    ⚡️(funsite) accelerate home page by overriding edX's view
    In the previous release, we cached the body of the home page template
    for authenticated users in an attempt to speed it up and found out that
    most of the time is spent in edX's view where they retrieve objects that
    we don't use in our home page. Adding our own view should further
    accelerate our home page.

--- funsite/views.py
+++ funsite/views.py
@@ -2,6 +2,13 @@
 
 from django.shortcuts import render
 
+from edxmako.shortcuts import render_to_response
+
+
+def index(request):
+    """A simple view to override edX's home page."""
+    return render_to_response("index.html")
+
 
 def render_static_template(request, page):
-    return render(request, 'funsite/static_templates/%s.html' % page, {})
+    return render(request, "funsite/static_templates/%s.html" % page, {})
