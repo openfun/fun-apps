8387c64abe868910c2201bd7c909bba830d28dec
jbpenrath <jb.penrath@gmail.com>
2020-11-23 10:40:00 +0100

    ✨(legal-acceptance) use a next query param to redirect after acceptance
    After legal acceptance, user was always redirect on the dashboard. Since we want
     to redirect user on Richie platform after login/registration, we need to be
     able to redirect user after legal acceptance. To achieve this we use the next
    param which is use widely elsewhere in EDX.

--- fun/lms/views.py
+++ fun/lms/views.py
@@ -7,10 +7,11 @@ from django.shortcuts import redirect
 
 def richie(request, redirect_to):
     """Extract params from route to redirect to the right Richie view"""
-    if (getattr(settings, "PLATFORM_RICHIE_URL", None) is not None):
-        return redirect("{richie_url:s}/{target:s}".format(
-            richie_url=settings.PLATFORM_RICHIE_URL,
-            target=redirect_to
-        ))
+    if getattr(settings, "PLATFORM_RICHIE_URL", None) is not None:
+        return redirect(
+            "{richie_url:s}/{target:s}".format(
+                richie_url=settings.PLATFORM_RICHIE_URL, target=redirect_to
+            )
+        )
 
     return redirect(reverse("dashboard"))
