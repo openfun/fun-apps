b39cb69bdf629f5de4e9a8c7686f675cf1e93b87
jbpenrath <jb.penrath@gmail.com>
2021-04-09 16:13:34 +0200

    ✨(funsite) update urls to redirect on richie
    legals, course, news and organizations views are now managed only on Richie CMS.
    So we have to redirect these OpenEdX views to their corresponding Richie view.

--- funsite/urls.py
+++ funsite/urls.py
@@ -2,13 +2,45 @@
 
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView, RedirectView
+from django.conf import settings
+from django.utils import translation
 
 from .views import index
 
-static_pages = ("about", "register_info")
-legal_pages = ("honor", "legal", "privacy", "tos", "charte")
+static_pages = ("register_info",)
+legal_pages = ("charte",)
 
-urls = (url(r"^$", index, name="root"),)
+richie_url = getattr(settings, "PLATFORM_RICHIE_URL", "")
+current_language = translation.get_language()
+
+cms_pages = {
+    "about": "{base_url:s}/{lang:s}/a-propos".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "cours": "{base_url:s}/{lang:s}/cours".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "honor": "{base_url:s}/{lang:s}/charte-utilisateurs".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "legal": "{base_url:s}/{lang:s}/mentions-legales".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "privacy": "{base_url:s}/{lang:s}/politique-de-confidentialite".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "tos": "{base_url:s}/{lang:s}/cgu".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "news": "{base_url:s}/{lang:s}/actualites".format(
+        base_url=richie_url, lang=current_language
+    ),
+    "universities": "{base_url:s}/{lang:s}/etablissements".format(
+        base_url=richie_url, lang=current_language
+    ),
+}
+
+urls = (url(r"^$", RedirectView.as_view(url=richie_url, permanent=True), name="root"),)
 
 urls += tuple(
     url(
@@ -27,6 +59,17 @@ urls += tuple(
             name=name,
         )
         for name in legal_pages
+    ]
+)
+
+urls += tuple(
+    [
+        url(
+            r"^{}/?$".format(name),
+            RedirectView.as_view(url=uri, permanent=True),
+            name=name,
+        )
+        for name, uri in cms_pages.items()
     ]
 )
 
